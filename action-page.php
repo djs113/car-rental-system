43d1ec9e1d24b975ca0e4556b4794ed054352cc2
djs113 <daniel.julius.sam.113@gmail.com>
2023-06-19 16:11:36 +0530

    completed action-page.php

--- action-page.php
+++ action-page.php
@@ -5,4 +5,28 @@
     $price = $_POST['price'];
     $quantity = $_POST['quantity'];
     $date_of_purchase = $_POST['date_of_purchase'];
+
+    $dbhost = 'localhost';
+    $dbuser = 'root';
+    $dbpass = '';
+    $dbname = 'books';
+    
+    $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+    if ($conn->connect_error)
+    {
+        die("Connection error".$conn->connect_error);
+    }
+
+    $sql = "INSERT INTO library VALUES ('$book_id', '$book_name', '$author', '$price', '$quantity', '$date_of_purchase')";
+
+    if ($conn->query($sql) === TRUE)
+    {
+        echo "Book successfully added<br>";
+    } else
+    {
+        echo "Error in addition of book<br>Error: ".$conn->error;
+    }
+
+    $conn->close();
 ?>

acf66c1e9f52f381caa0150f7960ff2b11056950
djs113 <daniel.julius.sam.113@gmail.com>
2023-06-19 16:03:40 +0530

    created action-page.php

---
+++ action-page.php
@@ -0,0 +1,8 @@
+<?php
+    $book_id = $_POST['book_id'];
+    $book_name = $_POST['book_name'];
+    $author = $_POST['author'];
+    $price = $_POST['price'];
+    $quantity = $_POST['quantity'];
+    $date_of_purchase = $_POST['date_of_purchase'];
+?>

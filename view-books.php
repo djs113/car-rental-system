1327b41203e9dcc963e3d6ab5234f343fa5a6899
djs113 <daniel.julius.sam.113@gmail.com>
2023-06-23 14:57:44 +0530

    connected db and wrote query to get user data

--- view-books.php
+++ view-books.php
@@ -1,3 +1,23 @@
 <?php
+    $dbhost = 'localhost';
+    $dbuser = 'root';
+    $dbpass = '';
+    $dbname = 'car-rental-system';
+   
+    $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+    if ($conn->connect_error)
+        die("Connection failed".$conn->connect_error);
+   
+    $sql = "SELECT * FROM user_details, user_emails, user_phone_numbers WHERE user_email.username = user_details.username = user_phone_numbers.username";
+    $res_array = mysqli_query($conn, $sql);
+
+    // echo '<table '
+    while ($result = mysqli_fetch_array($res_array))
+    {
+        
+    }
+
     
+
 ?>

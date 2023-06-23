5bc161a708be4dba7b119f70178ee105ad3080ba
djs113 <daniel.julius.sam.113@gmail.com>
2023-06-23 13:00:49 +0530

    updated user_emails and user_phone_numbers

--- action-page.php
+++ action-page.php
@@ -2,7 +2,7 @@
     $dbhost = 'localhost';
     $dbuser = 'root';
     $dbpass = '';
-    $dbname = 'car_rental_sytem';
+    $dbname = 'car_rental_system';
     
     $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
@@ -17,13 +17,12 @@
     $last_name = $_POST['last_name'];
     $email = $_POST['email'];
     $phone_number = $_POST['phone_number'];
-
-    $csql = "CREATE TABLE user_emails (email varchar(30) PRIMARY KEY, username VARCHAR(30) UNIQUE "
-    $sql = "INSERT INTO user_details VALUES ('$username', '$passwd', '$first_name', '$last_name')";
-    $sql .= "INSERT INTO user_emails VALUES ('$email', '$username')";
-    $sql .= "INSERT INTO user_phone_numbers VALUES ('$phone_number', '$username')";
-
-    if ($conn->multi_query($sql) === TRUE)
+    
+    //$sql = "INSERT INTO user_details VALUES ('$username', '$passwd', '$first_name', '$last_name')";
+    // $sql = "INSERT INTO user_emails VALUES ('$email', '$username')";
+    $sql = "INSERT INTO user_phone_numbers VALUES ('$phone_number', '$username')";
+    
+    if ($conn->query($sql) === TRUE)
     {
         echo "User successfully added<br>";
     } else

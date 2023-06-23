37d9a3388d3ec49e72e3bacfe9353452c501f8c3
djs113 <daniel.julius.sam.113@gmail.com>
2023-06-23 16:35:25 +0530

    added query to get user data

--- view-books.php
+++ view-books.php
@@ -9,15 +9,35 @@
     if ($conn->connect_error)
         die("Connection failed".$conn->connect_error);
    
-    $sql = "SELECT * FROM user_details, user_emails, user_phone_numbers WHERE user_email.username = user_details.username = user_phone_numbers.username";
+    $sql = "SELECT * FROM user_details INNER JOIN user_emails ON user_details.username = user_emails.username 
+            INNER JOIN user_phone_numbers ON user_details.username = user_phone_numbers.username";
     $res_array = mysqli_query($conn, $sql);
 
-    // echo '<table '
-    while ($result = mysqli_fetch_array($res_array))
+    echo '<table cellspacing="2" cellpadding="3">
+        <tr>
+            <th>Username</th>
+            <th>Password</th> 
+            <th>First Name</th> 
+            <th>Last Name</th> 
+            <th>Is Admin</th>
+            <th>Email</th>  
+            <th>Phone Number</th> 
+        </tr>';
+
+    while ($row = mysqli_fetch_array($res_array))
     {
-        
+        echo '<tr>
+            <td>'.$row["username"].'</td>
+            <td>'.$row["passwd"].'</td>
+            <td>'.$row["first_name"].'</td>
+            <td>'.$row["last_name"].'</td>
+            <td>'.$row["is_admin"].'</td>
+            <td>'.$row["email"].'</td>
+            <td>'.$row["phone_number"].'</td>
+        </tr>';
     }
 
-    
+    echo '</table>';
 
+    $conn->close();
 ?>

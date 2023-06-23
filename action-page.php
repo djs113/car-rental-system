18824c7cf96ec1460b8cfa2e3517bf0dd7d2f71f
djs113 <daniel.julius.sam.113@gmail.com>
2023-06-23 12:35:44 +0530

    updated action-page.php

--- action-page.php
+++ action-page.php
@@ -18,9 +18,12 @@
     $email = $_POST['email'];
     $phone_number = $_POST['phone_number'];
 
+    $csql = "CREATE TABLE user_emails (email varchar(30) PRIMARY KEY, username VARCHAR(30) UNIQUE "
     $sql = "INSERT INTO user_details VALUES ('$username', '$passwd', '$first_name', '$last_name')";
+    $sql .= "INSERT INTO user_emails VALUES ('$email', '$username')";
+    $sql .= "INSERT INTO user_phone_numbers VALUES ('$phone_number', '$username')";
 
-    if ($conn->query($sql) === TRUE)
+    if ($conn->multi_query($sql) === TRUE)
     {
         echo "User successfully added<br>";
     } else

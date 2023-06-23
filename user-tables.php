d83459b95ca86673f9e7effbc35e391bc8b66faa
djs113 <daniel.julius.sam.113@gmail.com>
2023-06-23 12:53:13 +0530

    added tables user_emails and user_phone_numbers

--- user-tables.php
+++ user-tables.php
@@ -15,6 +15,8 @@
 
     // $sql = "CREATE DATABASE car_rental_system";
     // $sql = "CREATE TABLE user_details (username VARCHAR(20) PRIMARY KEY, passwd VARCHAR(20), first_name VARCHAR(20), last_name VARCHAR(20), is_admin TINYINT DEFAULT 0)"; 
+    $sql = "CREATE TABLE user_emails (email varchar(30) PRIMARY KEY, username VARCHAR(30) UNIQUE NOT NULL)";
+    // $sql = "CREATE TABLE user_phone_numbers (phone_number INT PRIMARY KEY, username VARCHAR(30) UNIQUE NOT NULL)";
     // $sql = "INSERT INTO user_details VALUES (1, 'dfsa', 'sdvsfvs', 1200, 3, '2020-02-04')";
     // $sql = "UPDATE library SET book_id = 2 WHERE book_id = 1";
     // $sql = "INSERT INTO library VALUES (1, 'kasdlksjf', 'slkalja', 3888, 5, '2010-04-11')";

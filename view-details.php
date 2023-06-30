3ca7a6efb61aa927615c6e9e08cd84864698e03f
djs113 <daniel.julius.sam.113@gmail.com>
2023-06-30 12:06:23 +0530

    sql query to fetch complete user data

--- view-details.php
+++ view-details.php
@@ -1,3 +1,16 @@
 <?php
+    $dbhost = 'localhost';
+    $dbuser = 'root';
+    $dbpass = '';
+    $dbname = 'car_rental_system';
 
+    $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+    if ($conn->connect_error)
+        die("Connection failed<br>Connection Error: ".$conn->connect_error);
+  
+    $sql = "SELECT * FROM user_details INNER JOIN user_emails ON user_details.username = user_emails.username 
+    INNER JOIN user_phone_numbers ON user_details.username = user_phone_numbers.username";
+    
+    
 ?>

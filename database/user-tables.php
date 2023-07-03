19f0ffc8fab3200bdec610c591f7231863c843f4
djs113 <daniel.julius.sam.113@gmail.com>
2023-07-04 00:31:06 +0530

    added query for creation of table admins

--- database/user-tables.php
+++ database/user-tables.php
@@ -17,9 +17,10 @@
     // $sql = "CREATE DATABASE car_rental_system";
     
     // Table creation
-    // $sql = "CREATE TABLE user_details (username VARCHAR(20) PRIMARY KEY, passwd VARCHAR(20), first_name VARCHAR(20), last_name VARCHAR(20), is_admin TINYINT DEFAULT 0)"; 
+    // $sql = "CREATE TABLE user_details (username VARCHAR(20) PRIMARY KEY, passwd VARCHAR(40), first_name VARCHAR(20), last_name VARCHAR(20), is_admin TINYINT DEFAULT 0)"; 
     // $sql = "CREATE TABLE user_emails (email varchar(30) PRIMARY KEY, username VARCHAR(30) UNIQUE NOT NULL)";
     // $sql = "CREATE TABLE user_phone_numbers (phone_number INT PRIMARY KEY, username VARCHAR(30) UNIQUE NOT NULL)";
+    // $sql = "CREATE TABLE admins (username VARCHAR(20), passwd VARCHAR(40))";
     
     // $sql = "INSERT INTO user_details VALUES (1, 'dfsa', 'sdvsfvs', 1200, 3, '2020-02-04')";
     // $sql = "UPDATE library SET book_id = 2 WHERE book_id = 1";

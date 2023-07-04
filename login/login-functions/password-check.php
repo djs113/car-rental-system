9796db7f4acaec0375798d1a58e936fcf65f8ff6
djs113 <daniel.julius.sam.113@gmail.com>
2023-07-04 09:55:01 +0530

    fixed error in password-check.php

--- login/login-functions/password-check.php
+++ login/login-functions/password-check.php
@@ -5,10 +5,10 @@
         $enc_passwd = md5($passwd);
         
         $conn = mysqli_connect('localhost', 'root', '', 'car_rental_system');
-    
-        $qry = "SELECT passwd FROM '$table_name' WHERE username='$username'";
+        
+        $qry = "SELECT passwd FROM ".$table_name." WHERE username='$username'";
         $res_array = mysqli_query($conn, $qry);
-
+           
         while ($res = mysqli_fetch_array($res_array))
         {
             if ($enc_passwd == $res['passwd']) 

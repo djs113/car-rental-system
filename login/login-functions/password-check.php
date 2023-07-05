ac2895b8ec03ab57d9c2cb7e6fd4d3d07d3b467b
djs113 <daniel.julius.sam.113@gmail.com>
2023-07-05 23:17:56 +0530

    added message to indicate entering of invalid username

--- login/login-functions/password-check.php
+++ login/login-functions/password-check.php
@@ -6,13 +6,20 @@
         
         $qry = "SELECT passwd FROM $table_name WHERE username='$username'";
         $res_array = mysqli_query($conn, $qry);
-           
-        while ($res = mysqli_fetch_array($res_array))
+
+        $row_count = mysqli_num_rows($res_array);
+
+        if ($row_count == 0)
+            echo "Invalid username or password";
+        else
         {
-            if ($enc_passwd == $res['passwd']) 
-                echo "Successfully logged in"; 
-            else 
-                echo "Invalid username or password";
+            while ($res = mysqli_fetch_array($res_array))
+            {
+                if ($enc_passwd == $res['passwd']) 
+                    echo "Successfully logged in"; 
+                else 
+                    echo "Invalid username or password";
+            }
         }
     }
 ?>

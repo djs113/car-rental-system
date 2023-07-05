a5a20c09416fd686bb55d828f4b2ddecec22e35f
djs113 <daniel.julius.sam.113@gmail.com>
2023-07-05 20:28:57 +0530

    started working on displaying only the model names corresponding to the selected brand name

--- car-model/search-car-model/search-car-model-form.php
+++ car-model/search-car-model/search-car-model-form.php
@@ -3,6 +3,15 @@
         <title>
             Search Car Model
         </title>
+        <script type="text/javascript">
+                localStorage.setItem("brand_name", "null");
+                function setBrandValue()
+                {
+                    var brand_name = document.getElementById("brand_name").value;
+                    localStorage.setItem("brand_name", brand_name);
+                }
+                
+            </script>
     </head>
     <body>
         <form action="search-car-model.php" method="POST">
@@ -15,7 +24,7 @@
                 $res_array = mysqli_query($conn, $brand_qry);
 
                 echo '<label for="brand_name">Brand Name: </label>
-                <select name="brand_name" id="brand_name">';
+                <select onchange="setBrandValue()" name="brand_name" id="brand_name">';
 
                 while ($res = mysqli_fetch_array($res_array))
                 {
@@ -25,7 +34,15 @@
                 echo '</select>
                     <br><br>';
 
-                $model_qry = "SELECT model_name FROM vehicle_models";
+                $brand_name = '<script>document.write(localStorage.getItem("brand_name"));</script>';
+
+                echo $brand_name;
+
+                if ($model_name == "null")
+                    $model_qry = "SELECT model_name FROM vehicle_models"; 
+                else
+                    $model_qry = "SELECT model_name FROM vehicle_models WHERE brand_name='$brand_name'";
+
                 $res_array = mysqli_query($conn, $model_qry);
 
                 echo '<label for="model_name">Model Name: </label>
@@ -38,21 +55,7 @@
 
                 echo '</select>
                     <br><br>';
-            /*
-            <label for="brand name">Brand Name: </label>
-            <select name="brand_name" id="brand_name">
-                <option value="a">a</option>
-                <option value="b">b</option>
-            </select>
-            <br><br>
-
-            <label for="model name">Model Name: </label>
-            <select name="model_name" id="model_name">
-                <option value="a">a</option>
-                <option value="b">b</option>
-            </select>
-            <br><br>
-            */
+                
             ?>
             <input type="submit" value="Search Model" />
     </form>

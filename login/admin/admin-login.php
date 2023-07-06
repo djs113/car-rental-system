ed0cb1b7ce26d375cd0e19cdea4010463d5250ab
djs113 <daniel.julius.sam.113@gmail.com>
2023-07-06 19:49:51 +0530

    implemented session in admin-login.php

--- login/admin/admin-login.php
+++ login/admin/admin-login.php
@@ -1,7 +1,27 @@
 <?php
-    require '/opt/lampp/htdocs/car-rental-system/login/login-functions/password-check.php';
-    require '/opt/lampp/htdocs/car-rental-system/common-functions.php';
+    session_start();
+
+    $conn = mysqli_connect('localhost', 'root', '', 'car_rental_system');
+
+    if ($conn->connect_error)
+        die("Connection error".$conn->connect_error);
+
+    $username = $_POST['username'];
+    $passwd = $_POST['passwd'];
+    $enc_passwd = md5($passwd);
     
-    $conn = dbConnection();
-    passwordCheck('admins', $conn);
+    $qry = "SELECT * FROM admins WHERE username='$username' AND passwd='$enc_passwd'";
+    $res_array = mysqli_query($conn, $qry);
+
+    $row_count = mysqli_num_rows($res_array);
+    $res = mysqli_fetch_array($res_array);
+
+    if ($res)
+    {
+        $_SESSION['login_admin'] = $res['username'];
+        header("location:/car-rental-system/registered-user/view-user/view-all-users.php");
+    } else
+    {
+        header("location:/car-rental-system/login/admin/admin-login-page.html");
+    }
 ?>

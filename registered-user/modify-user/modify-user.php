1f78423ee6051736a13e3fc76b64a64066c0ccf8
djs113 <daniel.julius.sam.113@gmail.com>
2023-07-06 18:07:39 +0530

    completed modify-user.php

---
+++ registered-user/modify-user/modify-user.php
@@ -0,0 +1,18 @@
+<?php
+    $conn = mysqli_connect('localhost', 'root', '', 'car_rental_system');
+
+    if ($conn->connect_error)
+        die("Connection error".$conn->connect_error);
+
+    $username = $_POST['username'];
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $passwd = $_POST['passwd'];
+  
+    $qry = "UPDATE user_details SET first_name = '$first_name', last_name = '$last_name', passwd = '$passwd' WHERE username = '$username'";
+    
+    if ($conn->query($qry) == TRUE)
+        echo "User profile successfully modified.<br>";
+    else
+        echo "Error in modification of user profile.<br>Error: ".$conn->error;
+?>

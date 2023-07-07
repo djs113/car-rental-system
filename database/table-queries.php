bc670405a3f5c85adfa32ecab85e42e8e58d70b7
djs113 <daniel.julius.sam.113@gmail.com>
2023-07-07 21:46:07 +0530

    style: add whitespace between some headings and queries

--- database/table-queries.php
+++ database/table-queries.php
@@ -35,6 +35,7 @@
             // $sql = "INSERT INTO admins VALUES ('admin', '21232f297a57a5a743894a0e4a801fc3')";
     
     // Vehicle tables
+        
         // Table creation
             // $sql = "CREATE TABLE vehicle_models (model_id INT PRIMARY KEY AUTO_INCREMENT, brand_name VARCHAR(20), model_name VARCHAR(30), vehicle_type VARCHAR(20), hour_price INT, day_price INT, week_price INT, month_price INT)";
             // $sql = "CREATE TABLE vehicles (registration_number VARCHAR(11) PRIMARY KEY, vehicle_color VARCHAR(20), is_booked TINYINT DEFAULT 0, model_id int, FOREIGN KEY (model_id) REFERENCES vehicle_models(model_id))";
@@ -45,7 +46,7 @@
             //$sql = "INSERT INTO engine_numbers VALUES ('343BDSN452', 'MN43LLJ23')";
     
     // Booking tables
-    
+
         // Table creation
             // $sql = "CREATE TABLE card_booking_details (booking_id INT PRIMARY KEY, is_booked_for VARCHAR(20), booking_duration INT, pick_up_date DATE, pick_up_time TIME, payment_amount INT, payment_time DATETIME, card_id BIGINT, registration_number VARCHAR(11), FOREIGN KEY (registration_number) REFERENCES vehicles(registration_number), FOREIGN KEY (card_id) REFERENCES user_cards(card_id))";
             // $sql = "CREATE TABLE cash_booking_details (booking_id INT PRIMARY KEY, is_booked_for VARCHAR(20), booking_duration INT, pick_up_date DATE, pick_up_time TIME, payment_amount INT, payment_time DATETIME, username VARCHAR(20), registration_number VARCHAR(11), FOREIGN KEY (registration_number) REFERENCES vehicles(registration_number), FOREIGN KEY (username) REFERENCES user_details(username))";
@@ -55,10 +56,12 @@
                 // $sql = "INSERT INTO cash_booking_details VALUES (45, 'weeks', 2, '2023-03-05', '10:00:04', 5000, '2023-03-03 11:03:05', 'aa', 'MN43LLJ23')";
 
     // Card tables
+
         // Table creation
             // $sql = "CREATE TABLE card_details (card_number BIGINT PRIMARY KEY, name_on_card VARCHAR(30), expiry_date DATE)"; 
             // $sql = "CREATE TABLE user_cards (card_id BIGINT PRIMARY KEY, card_name VARCHAR(20), username VARCHAR(20), card_number BIGINT, FOREIGN KEY (username) REFERENCES user_details(username), FOREIGN KEY (card_number) REFERENCES card_details(card_number))";    
-        // Record Insertion
+        
+            // Record Insertion
             // $sql = "INSERT INTO card_details VALUES (2343131231, 'abc', '2024-04-06')";
             // $sql = "INSERT INTO user_cards VALUES (5643234, 'card', 'aa', 2343131231)";
 

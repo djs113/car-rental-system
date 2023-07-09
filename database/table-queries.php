cf8c2b22af244f0f9313fdaba71ac524fe0166f2
djs113 <daniel.julius.sam.113@gmail.com>
2023-07-09 12:55:26 +0530

    style: uncommented sql queries

--- database/table-queries.php
+++ database/table-queries.php
@@ -14,58 +14,66 @@
     echo "Connection successful<br>";
     
     // Database creation
-    // $sql = "CREATE DATABASE car_rental_system";
-   
-    // User tables
+        // $sql = "CREATE DATABASE car_rental_system_1;";
+    
+    // Selecting database
+        // $sql .= "USE car_rental_system_1;";
+    
+        // User tables
+            // Table creation
+                $sql = "CREATE TABLE user_details (username VARCHAR(20) PRIMARY KEY, passwd VARCHAR(40), first_name VARCHAR(20), last_name VARCHAR(20), is_admin TINYINT DEFAULT 0);"; 
+                $sql .= "CREATE TABLE user_emails (email varchar(30) PRIMARY KEY, username VARCHAR(30) UNIQUE NOT NULL);";
+                $sql .= "CREATE TABLE user_phone_numbers (phone_number INT PRIMARY KEY, username VARCHAR(30) UNIQUE NOT NULL);";
 
-        // Table creation
-            // $sql = "CREATE TABLE user_details (username VARCHAR(20) PRIMARY KEY, passwd VARCHAR(40), first_name VARCHAR(20), last_name VARCHAR(20), is_admin TINYINT DEFAULT 0)"; 
-            // $sql = "CREATE TABLE user_emails (email varchar(30) PRIMARY KEY, username VARCHAR(30) UNIQUE NOT NULL)";
-            // $sql = "CREATE TABLE user_phone_numbers (phone_number INT PRIMARY KEY, username VARCHAR(30) UNIQUE NOT NULL)";
-            // $sql = "CREATE TABLE admins (username VARCHAR(20), passwd VARCHAR(40))";
-
-        // $sql = "ALTER TABLE user_phone_numbers ADD CONSTRAINT FOREIGN KEY (username) REFERENCES user_details(username)";
-        // $sql = "ALTER TABLE user_emails ADD CONSTRAINT FOREIGN KEY (username) REFERENCES user_details(username)";
+            // Table alteration
+                $sql .= "ALTER TABLE user_phone_numbers ADD CONSTRAINT FOREIGN KEY (username) REFERENCES user_details(username);";
+                $sql .= "ALTER TABLE user_emails ADD CONSTRAINT FOREIGN KEY (username) REFERENCES user_details(username);";
+            
+                // Record Insertion
+                $sql .= "INSERT INTO `user_details` (`username`, `passwd`, `first_name`, `last_name`) VALUES('aa', '4124bc0a9335c27f086f24ba207a4912', 'aa', 'bb');";
+                $sql .= "INSERT INTO `user_emails` (`email`, `username`) VALUES('a@gmail.com', 'aa');";
+                $sql .= "INSERT INTO `user_phone_numbers` (`phone_number`, `username`) VALUES(2313312321323, 'aa');";
 
     // Admin table 
         // Table creation
-            // $sql = "CREATE TABLE admins (username VARCHAR(20) PRIMARY KEY, passwd VARCHAR(40))"; 
+            $sql .= "CREATE TABLE admins (username VARCHAR(20) PRIMARY KEY, passwd VARCHAR(40));"; 
         
             // Record insertion
-            // $sql = "INSERT INTO admins VALUES ('admin', '21232f297a57a5a743894a0e4a801fc3')";
+                $sql .= "INSERT INTO admins VALUES ('admin', '21232f297a57a5a743894a0e4a801fc3');";
     
     // Vehicle tables
         
         // Table creation
-            // $sql = "CREATE TABLE vehicle_models (model_id INT PRIMARY KEY AUTO_INCREMENT, brand_name VARCHAR(20), model_name VARCHAR(30), vehicle_type VARCHAR(20), hour_price INT, day_price INT, week_price INT, month_price INT)";
-            // $sql = "CREATE TABLE vehicles (registration_number VARCHAR(11) PRIMARY KEY, vehicle_color VARCHAR(20), is_booked TINYINT DEFAULT 0, model_id int, FOREIGN KEY (model_id) REFERENCES vehicle_models(model_id))";
-            // $sql = "CREATE TABLE engine_numbers (engine_number VARCHAR(20) PRIMARY KEY, registration_number VARCHAR(11) UNIQUE, FOREIGN KEY (registration_number) REFERENCES vehicles(registration_number))";
+                $sql .= "CREATE TABLE vehicle_models (model_id INT PRIMARY KEY AUTO_INCREMENT, brand_name VARCHAR(20), model_name VARCHAR(30), vehicle_type VARCHAR(20), hour_price INT, day_price INT, week_price INT, month_price INT);";
+                $sql .= "CREATE TABLE vehicles (registration_number VARCHAR(11) PRIMARY KEY, vehicle_color VARCHAR(20), is_booked TINYINT DEFAULT 0, model_id int, FOREIGN KEY (model_id) REFERENCES vehicle_models(model_id));";
+                $sql .= "CREATE TABLE engine_numbers (engine_number VARCHAR(20) PRIMARY KEY, registration_number VARCHAR(11) UNIQUE, FOREIGN KEY (registration_number) REFERENCES vehicles(registration_number));";
 
         // Record Insertion
-            //$sql = "INSERT INTO vehicles VALUES ('MN43LLJ23', 'lkcvlv', 0, 4)";
-            //$sql = "INSERT INTO engine_numbers VALUES ('343BDSN452', 'MN43LLJ23')";
+            $sql = "INSERT INTO vehicle_models VALUES (4, 'xcvfs', 'vcvsdfsf', 'cvxsdfs', 300, 1000, 2300, 7000);";
+            $sql .= "INSERT INTO vehicles VALUES ('MN43LLJ23', 'lkcvlv', 0, 4);";
+            $sql .= "INSERT INTO engine_numbers VALUES ('343BDSN452', 'MN43LLJ23');";
     
-    // Booking tables
-
-        // Table creation
-            // $sql = "CREATE TABLE card_booking_details (booking_id INT PRIMARY KEY, is_booked_for VARCHAR(20), booking_duration INT, pick_up_date DATE, pick_up_time TIME, payment_amount INT, payment_time DATETIME, card_id BIGINT, registration_number VARCHAR(11), FOREIGN KEY (registration_number) REFERENCES vehicles(registration_number), FOREIGN KEY (card_id) REFERENCES user_cards(card_id))";
-            // $sql = "CREATE TABLE cash_booking_details (booking_id INT PRIMARY KEY, is_booked_for VARCHAR(20), booking_duration INT, pick_up_date DATE, pick_up_time TIME, payment_amount INT, payment_time DATETIME, username VARCHAR(20), registration_number VARCHAR(11), FOREIGN KEY (registration_number) REFERENCES vehicles(registration_number), FOREIGN KEY (username) REFERENCES user_details(username))";
-        
-            // Record insertion
-                // $sql = "INSERT INTO card_booking_details VALUES (22, 'hours', 5, '2023-06-09', '09:07:00', 1000, '2023-06-08 14:05:06', 5643234, 'MN43LLJ23')";
-                // $sql = "INSERT INTO cash_booking_details VALUES (45, 'weeks', 2, '2023-03-05', '10:00:04', 5000, '2023-03-03 11:03:05', 'aa', 'MN43LLJ23')";
-
     // Card tables
 
         // Table creation
-            // $sql = "CREATE TABLE card_details (card_number BIGINT PRIMARY KEY, name_on_card VARCHAR(30), expiry_date DATE)"; 
-            // $sql = "CREATE TABLE user_cards (card_id BIGINT PRIMARY KEY, card_name VARCHAR(20), username VARCHAR(20), card_number BIGINT, FOREIGN KEY (username) REFERENCES user_details(username), FOREIGN KEY (card_number) REFERENCES card_details(card_number))";    
+            $sql .= "CREATE TABLE card_details (card_number BIGINT PRIMARY KEY, name_on_card VARCHAR(30), expiry_date DATE);"; 
+            $sql .= "CREATE TABLE user_cards (card_id BIGINT PRIMARY KEY, card_name VARCHAR(20), username VARCHAR(20), card_number BIGINT, FOREIGN KEY (username) REFERENCES user_details(username), FOREIGN KEY (card_number) REFERENCES card_details(card_number));";    
         
             // Record Insertion
-            // $sql = "INSERT INTO card_details VALUES (2343131231, 'abc', '2024-04-06')";
-            // $sql = "INSERT INTO user_cards VALUES (5643234, 'card', 'aa', 2343131231)";
+            $sql .= "INSERT INTO card_details VALUES (2343131231, 'abc', '2024-04-06');";
+            $sql .= "INSERT INTO user_cards VALUES (5643234, 'card', 'aa', 2343131231);";
 
-    if ($conn->query($sql) === TRUE)
+    // Booking tables
+
+        // Table creation
+            $sql .= "CREATE TABLE card_booking_details (booking_id INT PRIMARY KEY, is_booked_for VARCHAR(20), booking_duration INT, pick_up_date DATE, pick_up_time TIME, payment_amount INT, payment_time DATETIME, card_id BIGINT, registration_number VARCHAR(11), FOREIGN KEY (registration_number) REFERENCES vehicles(registration_number), FOREIGN KEY (card_id) REFERENCES user_cards(card_id));";
+            $sql .= "CREATE TABLE cash_booking_details (booking_id INT PRIMARY KEY, is_booked_for VARCHAR(20), booking_duration INT, pick_up_date DATE, pick_up_time TIME, payment_amount INT, payment_time DATETIME, username VARCHAR(20), registration_number VARCHAR(11), FOREIGN KEY (registration_number) REFERENCES vehicles(registration_number), FOREIGN KEY (username) REFERENCES user_details(username));";
+        
+            // Record insertion
+                $sql .= "INSERT INTO card_booking_details VALUES (22, 'hours', 5, '2023-06-09', '09:07:00', 1000, '2023-06-08 14:05:06', 5643234, 'MN43LLJ23');";
+                $sql .= "INSERT INTO cash_booking_details VALUES (45, 'weeks', 2, '2023-03-05', '10:00:04', 5000, '2023-03-03 11:03:05', 'aa', 'MN43LLJ23');";
+
+    if ($conn->multi_query($sql) === TRUE)
     {
         echo "Query successful";
     } else 

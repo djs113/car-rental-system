1cff524569ef1fe0ca45d270fd5e9ef299b5afec
djs113 <daniel.julius.sam.113@gmail.com>
2023-08-04 00:19:41 +0530

    feat: completed available vehicle search functionality

--- registered-user/booking/vehicle-search/vehicle-search-script.php
+++ registered-user/booking/vehicle-search/vehicle-search-script.php
@@ -1,6 +1,4 @@
 <?php
-    session_start();
-
     $conn = mysqli_connect('localhost', 'root', '', 'car_rental_system');
 
     if ($conn->connect_error)
@@ -13,43 +11,82 @@
     $given_drop_off_time = $_POST['drop_off_time'];
 
     // unbooked vehicle details
+
     $qry = "SELECT vehicle_models.model_name, vehicle_models.brand_name FROM vehicles LEFT JOIN vehicle_models ON vehicles.model_id = vehicle_models.model_id WHERE vehicles.is_booked = 0 GROUP BY vehicle_models.model_id";
     $unbooked_vehicles_array = mysqli_query($conn, $qry);
 
-    echo '
-        <table border="1">
-            <th>Model Name</th>
-            <th>Brand Name</th>
-    ';
+    
+    // currently booked vehicle details (that satisfies time period given by user)
 
-    while ($res = mysqli_fetch_array($unbooked_vehicles_array))
+    $qry = "SELECT vehicle_models.model_name, vehicle_models.brand_name FROM vehicles LEFT JOIN vehicle_models ON 
+            vehicles.model_id = vehicle_models.model_id WHERE vehicles.is_booked = 0 GROUP BY vehicle_models.model_id
+            UNION
+            SELECT DISTINCT vehicle_models.model_name, vehicle_models.brand_name from vehicles LEFT JOIN cash_booking_details 
+            ON vehicles.registration_number = cash_booking_details.registration_number LEFT JOIN vehicle_models ON 
+            vehicles.model_id = vehicle_models.model_id WHERE ('$given_drop_off_date' < cash_booking_details.pick_up_date OR 
+            '$given_pick_up_date' > cash_booking_details.drop_off_date) 
+            UNION 
+            SELECT DISTINCT vehicle_models.model_name, 
+            vehicle_models.brand_name from cash_booking_details WHERE '$given_drop_off_date' < pick_up_date OR 
+            '$given_pick_up_date' > drop_off_date;";
+
+    $qry = "SELECT vehicle_models.model_name, vehicle_models.brand_name FROM vehicles LEFT JOIN vehicle_models ON 
+            vehicles.model_id = vehicle_models.model_id WHERE vehicles.is_booked = 0 GROUP BY vehicle_models.model_id
+            UNION
+            SELECT DISTINCT vehicle_models.model_name, vehicle_models.brand_name FROM vehicles LEFT JOIN card_booking_details
+            ON vehicles.registration_number = card_booking_details.registration_number LEFT JOIN vehicle_models ON 
+            vehicles.model_id = vehicle_models.model_id WHERE ('$given_drop_off_date' < card_booking_details.pick_up_date OR 
+            '$given_pick_up_date' > card_booking_details.drop_off_date) 
+            UNION 
+            SELECT DISTINCT vehicle_models.model_name, vehicle_models.brand_name from vehicles LEFT JOIN 
+            card_booking_details ON vehicles.registration_number = card_booking_details.registration_number LEFT JOIN 
+            vehicle_models ON vehicles.model_id = vehicle_models.model_id WHERE 
+            ('$given_drop_off_date' < card_booking_details.pick_up_date OR 
+            '$given_pick_up_date' > card_booking_details.drop_off_date)";
+
+    $vehicles_array = mysqli_query($conn, $qry);
+
+    if (mysqli_num_rows($vehicles_array) == 0)
+    {
+        echo 'No available vehicles for this time period
+              <br><br>
+              <button><a href="/car-rental-system/registered-user/booking/vehicle-search/vehicle-search-form.php">Search again</a></button>
+        ';
+
+    } else
     {
         echo '
-            <tr>
-                <td>'.$res[0].'</td>
-                <td>'.$res[1].'</td>
-                <td><a href="book-individual-vehicle.php">Book</a></td>
-            </tr>
+            <table border="1">
+                <th>Model Name</th>
+                <th>Brand Name</th>
         ';
+
+        while ($res = mysqli_fetch_row($vehicles_array))
+            {
+                echo '
+                    <tr>
+                        <td>'.$res[0].'</td>
+                        <td>'.$res[1].'</td>
+                        <td><a href="book-individual-vehicle.php">Book</a></td>
+                    </tr>
+                ';
+            }
     }
+    /*
+    while ($res = mysqli_fetch_row($unbooked_vehicles_array))
+        {
+            echo '
+                <tr>
+                    <td>'.$res[0].'</td>
+                    <td>'.$res[1].'</td>
+                    <td><a href="book-individual-vehicle.php">Book</a></td>
+                </tr>
+            ';
+        }
+    */
 
     echo '
         </table>
     ';
 
-    /* 
-    // currently booked vehicle details (that satisfies time period given by user)
-    $qry = "SELECT DISTINCT vehicles.registration_number from vehicles LEFT JOIN card_booking_details ON vehicles.registration_number = card_booking_details.registration_number WHERE (given_drop_off_date < pick_up_date OR given_pick_up_date > drop_off_date)";
-    $card_booked_vehicles_array = mysqli_query($conn, $qry);
-
-    $qry = "SELECT DISTINCT registration_number from cash_booking_details WHERE given_drop_off_date < pick_up_date OR given_pick_up_date > drop_off_date;";
-    $cash_booked_vehicles_array = mysqli_query($conn, $sql);
-
-    $booked_vehicles_array = array($card_booked_vehicles_array, $cash_booked_vehicles_array);
-
-    foreach ($booked_vehicles_array as $vehicles_array)
-    {
-
-    }
-    */
 ?>

3a87c05d84c33c656d905ae857050ba3827a693d
djs113 <daniel.julius.sam.113@gmail.com>
2023-08-04 00:18:55 +0530

    feat: completed add car functionality

--- car/add-car/add-car-form.php
+++ car/add-car/add-car-form.php
@@ -15,23 +15,63 @@
     ?>
     <body>
         <h2><u>Add Car</u></h2>
-        <form action="add-car.php" method="POST">
-            <label for="brand name">Brand Name: </label><input type="text" name="brand_name" id="brand_name" />
-            <br><br>
+        <?php
+            session_start();
 
-            <label for="model name">Model Name: </label><input type="text" name="model_name" id="model_name" />
-            <br><br>
+            if (!isset($_SESSION['login_admin']))
+            {
+                header("location:/car-rental-system/admin/admin-login/admin-login.php");
+                exit;
+            }
+        
+            $conn = mysqli_connect('localhost', 'root', '', 'car_rental_system');
+        
+            if ($conn->connect_error)
+                die("Connection error".$conn->connect_error);
 
-            <label for="registration number">Registration Number: </label><input type="text" name="registration_number" id="registration_number" />
-            <br><br>
+            $qry = "SELECT brand_name FROM vehicle_models";
+            $res_array = mysqli_query($conn, $qry);
 
-            <label for="engine number">Engine Number: </label><input type="text" name="engine_number" id="engine_number" />
-            <br><br>
+            echo '
+                <form action="add-car-script.php" method="POST">
+                    <label for="brand name">Brand Name: </label>
+                    <select name="brand_name" id="brand_name">      
+            ';
 
-            <label for="vehicle color">Vehicle Color: </label><input type="text" name="vehicle_color" id="vehicle_color" />
-            <br><br>
+            while ($res = mysqli_fetch_array($res_array))
+                echo '<option value="'.$res['brand_name'].'">'.$res['brand_name'].'</option>';
+            
+            $qry = "SELECT model_name FROM vehicle_models";
+            $res_array = mysqli_query($conn, $qry);
 
-            <input type="submit" value="Add Vehicle" />
-        </form>
+            echo '
+                </select>
+                <br><br>
+
+                <label for="model name">Model Name: </label>
+                <select name="model_name" id="model_name"> 
+            ';
+            
+            while ($res = mysqli_fetch_array($res_array))
+                echo '<option value="'.$res['model_name'].'">'.$res['model_name'].'</option>';
+            
+            echo '  
+                    </select>
+                    <br><br>
+
+                    <label for="registration number">Registration Number: </label><input type="text" name="registration_number" id="registration_number" />
+                        <br><br>
+
+                    <label for="engine number">Engine Number: </label><input type="text" name="engine_number" id="engine_number" />
+                    <br><br>
+
+                    <label for="vehicle color">Vehicle Color: </label><input type="text" name="vehicle_color" id="vehicle_color" />
+                    <br><br>
+
+                    <input type="submit" value="Add Vehicle" />
+                </form>
+            ';
+        ?>
+        <button><a href="/car-rental-system/admin/admin-home/admin-home-page.php">Home Page</a></button>
     </body>
 </html>
---
+++ car/add-car/add-car-script.php
@@ -0,0 +1,44 @@
+<?php
+    session_start();
+
+    if (!isset($_SESSION['login_admin']))
+    {
+        header("location:/car-rental-system/admin/admin-login/admin-login.php");
+        exit;
+    }
+
+    $conn = mysqli_connect('localhost', 'root', '', 'car_rental_system');
+
+    if ($conn->connect_error)
+        die("Connection error".$conn->connect_error);
+
+    $brand_name = $_POST['brand_name'];
+    $model_name = $_POST['model_name'];
+    $registration_number = $_POST['registration_number'];
+    $engine_number = $_POST['engine_number'];
+    $vehicle_color = $_POST['vehicle_color'];
+
+    $qry = "SELECT model_id FROM vehicle_models WHERE brand_name = '$brand_name' AND 
+            model_name = '$model_name'";
+    
+    $res_array = mysqli_query($conn, $qry);
+
+    $res = mysqli_fetch_array($res_array);
+    $model_id = $res['model_id'];
+
+    $qry = "INSERT INTO vehicles (registration_number, vehicle_color, model_id) VALUES 
+    ('$registration_number', '$vehicle_color', '$model_id');";
+    
+    $qry .= "INSERT INTO engine_numbers VALUES ('$engine_number', '$registration_number');";
+    
+    if ($conn->multi_query($qry) == TRUE)
+        echo 'Successfully added car!';
+    else
+        echo 'Error in addition of car';
+
+    echo '
+        <br><br>
+        <button><a href="/car-rental-system/car/add-car/add-car-form.php">Go back</a></button>
+    ';
+    
+?>

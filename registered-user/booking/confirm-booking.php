76a54440e2509cc834ca3045894ed844659a7d23
djs113 <daniel.julius.sam.113@gmail.com>
2023-08-05 02:57:28 +0530

    feat: price calculation logic

--- registered-user/booking/confirm-booking.php
+++ registered-user/booking/confirm-booking.php
@@ -15,9 +15,9 @@
     $model_id = $_POST['model_id'];
 
     $pick_up_date = new DateTime($_POST['pick_up_date']);
-    $pick_up_time = new DateTime($_POST['pick_up_time']);
+    $pick_up_time = strtotime($_POST['pick_up_date']." ".$_POST['pick_up_time'].":00");
     $drop_off_date = new DateTime($_POST['drop_off_date']);
-    $drop_off_time = new DateTime($_POST['drop_off_time']);
+    $drop_off_time = strtotime($_POST['drop_off_date']." ".$_POST['drop_off_time'].":");
 
     $qry = "SELECT brand_name, model_name, hour_price, day_price, week_price, month_price FROM vehicle_models WHERE model_id = $model_id";
     
@@ -33,13 +33,25 @@
     $month_diff = $date_diff->m;
     $day_diff = $day_diff->d;
 
-    // month price
-    
     if ($month_diff != 0)
         $payable_amount += $res['month_price'] * $month_diff;
     
     if ($day_diff != 0)
+    {
+        if ($day_diff >= 7)
+        {
+            $week_diff = $day_diff / 7;
+            $payable_amount += $res['week_price'] * $week_diff;
+            
+            $day_diff = $day_diff % 7;
+        }
+
         $payable_amount += $res['day_price'] * $day_diff;
+    } else 
+    {
+        $hour_diff = ($drop_off_time - $pick_up_time) / 3600;
+        $payable_amount += $res['hour_price'] * $hour_diff;
+    }
 
     echo '
         <label for="brand_name">Brand Name: </label>'.$res[0].'
@@ -58,6 +70,9 @@
         <br><br>
 
         <label for="drop_off_time">Drop off time: </label>'.$_POST['drop_off_time'].'
+        <br><br>
+
+        <label for="payable_amount">Payable amount: </label>'.$payable_amount.'
         <br><br>
     ';
 ?>

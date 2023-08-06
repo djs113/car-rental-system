bf3c819570bafdb344db8b620f5abdaeea4d988a
djs113 <daniel.julius.sam.113@gmail.com>
2023-08-06 23:40:51 +0530

    chore: updated database backup

--- database/backups/car_rental_system.sql
+++ database/backups/car_rental_system.sql
@@ -1,23 +1,27 @@
 -- phpMyAdmin SQL Dump
--- version 3.3.9
--- http://www.phpmyadmin.net
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 10, 2023 at 05:15 AM
--- Server version: 5.5.8
--- PHP Version: 5.3.5
+-- Generation Time: Aug 06, 2023 at 08:10 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `car_rental_system`
 --
+CREATE DATABASE IF NOT EXISTS `car_rental_system` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `car_rental_system`;
 
 -- --------------------------------------------------------
 
@@ -25,18 +29,16 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- Table structure for table `admins`
 --
 
-CREATE TABLE IF NOT EXISTS `admins` (
+CREATE TABLE `admins` (
   `username` varchar(20) NOT NULL,
-  `passwd` varchar(40) DEFAULT NULL,
-  PRIMARY KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `passwd` varchar(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `admins`
 --
 
-INSERT INTO `admins` (`username`, `passwd`) VALUES
-('admin', '21232f297a57a5a743894a0e4a801fc3');
+INSERT INTO `admins` (`username`, `passwd`) VALUES('admin', '21232f297a57a5a743894a0e4a801fc3');
 
 -- --------------------------------------------------------
 
@@ -44,27 +46,23 @@ INSERT INTO `admins` (`username`, `passwd`) VALUES
 -- Table structure for table `card_booking_details`
 --
 
-CREATE TABLE IF NOT EXISTS `card_booking_details` (
+CREATE TABLE `card_booking_details` (
   `booking_id` int(11) NOT NULL,
-  `is_booked_for` varchar(20) DEFAULT NULL,
-  `booking_duration` int(11) DEFAULT NULL,
   `pick_up_date` date DEFAULT NULL,
   `pick_up_time` time DEFAULT NULL,
+  `drop_off_date` date DEFAULT NULL,
+  `drop_off_time` time DEFAULT NULL,
   `payment_amount` int(11) DEFAULT NULL,
   `payment_time` datetime DEFAULT NULL,
   `card_id` bigint(20) DEFAULT NULL,
-  `registration_number` varchar(11) DEFAULT NULL,
-  PRIMARY KEY (`booking_id`),
-  KEY `registration_number` (`registration_number`),
-  KEY `card_id` (`card_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `registration_number` varchar(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `card_booking_details`
 --
 
-INSERT INTO `card_booking_details` (`booking_id`, `is_booked_for`, `booking_duration`, `pick_up_date`, `pick_up_time`, `payment_amount`, `payment_time`, `card_id`, `registration_number`) VALUES
-(22, 'hours', 5, '2023-06-09', '09:07:00', 1000, '2023-06-08 14:05:06', 5643234, 'MN43LLJ23');
+INSERT INTO `card_booking_details` (`booking_id`, `pick_up_date`, `pick_up_time`, `drop_off_date`, `drop_off_time`, `payment_amount`, `payment_time`, `card_id`, `registration_number`) VALUES(22, '2023-06-09', '09:07:00', '2023-06-13', '19:07:00', 1000, '2023-06-08 14:05:06', 5643234, 'MN43LLJ23');
 
 -- --------------------------------------------------------
 
@@ -72,19 +70,17 @@ INSERT INTO `card_booking_details` (`booking_id`, `is_booked_for`, `booking_dura
 -- Table structure for table `card_details`
 --
 
-CREATE TABLE IF NOT EXISTS `card_details` (
+CREATE TABLE `card_details` (
   `card_number` bigint(20) NOT NULL,
   `name_on_card` varchar(30) DEFAULT NULL,
-  `expiry_date` date DEFAULT NULL,
-  PRIMARY KEY (`card_number`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `expiry_date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `card_details`
 --
 
-INSERT INTO `card_details` (`card_number`, `name_on_card`, `expiry_date`) VALUES
-(2343131231, 'abc', '2024-04-06');
+INSERT INTO `card_details` (`card_number`, `name_on_card`, `expiry_date`) VALUES(2343131231, 'abc', '2024-04-06');
 
 -- --------------------------------------------------------
 
@@ -92,27 +88,23 @@ INSERT INTO `card_details` (`card_number`, `name_on_card`, `expiry_date`) VALUES
 -- Table structure for table `cash_booking_details`
 --
 
-CREATE TABLE IF NOT EXISTS `cash_booking_details` (
+CREATE TABLE `cash_booking_details` (
   `booking_id` int(11) NOT NULL,
-  `is_booked_for` varchar(20) DEFAULT NULL,
-  `booking_duration` int(11) DEFAULT NULL,
   `pick_up_date` date DEFAULT NULL,
   `pick_up_time` time DEFAULT NULL,
   `payment_amount` int(11) DEFAULT NULL,
   `payment_time` datetime DEFAULT NULL,
   `username` varchar(20) DEFAULT NULL,
   `registration_number` varchar(11) DEFAULT NULL,
-  PRIMARY KEY (`booking_id`),
-  KEY `registration_number` (`registration_number`),
-  KEY `username` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `drop_off_date` date DEFAULT NULL,
+  `drop_off_time` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `cash_booking_details`
 --
 
-INSERT INTO `cash_booking_details` (`booking_id`, `is_booked_for`, `booking_duration`, `pick_up_date`, `pick_up_time`, `payment_amount`, `payment_time`, `username`, `registration_number`) VALUES
-(45, 'weeks', 2, '2023-03-05', '10:00:04', 5000, '2023-03-03 11:03:05', 'aa', 'MN43LLJ23');
+INSERT INTO `cash_booking_details` (`booking_id`, `pick_up_date`, `pick_up_time`, `payment_amount`, `payment_time`, `username`, `registration_number`, `drop_off_date`, `drop_off_time`) VALUES(45, '2023-03-05', '10:00:04', 5000, '2023-03-03 11:03:05', 'aa', 'MN43LLJ23', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -120,19 +112,18 @@ INSERT INTO `cash_booking_details` (`booking_id`, `is_booked_for`, `booking_dura
 -- Table structure for table `engine_numbers`
 --
 
-CREATE TABLE IF NOT EXISTS `engine_numbers` (
+CREATE TABLE `engine_numbers` (
   `engine_number` varchar(20) NOT NULL,
-  `registration_number` varchar(11) DEFAULT NULL,
-  PRIMARY KEY (`engine_number`),
-  UNIQUE KEY `registration_number` (`registration_number`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `registration_number` varchar(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `engine_numbers`
 --
 
-INSERT INTO `engine_numbers` (`engine_number`, `registration_number`) VALUES
-('343BDSN452', 'MN43LLJ23');
+INSERT INTO `engine_numbers` (`engine_number`, `registration_number`) VALUES('BNN6SN452', 'CVB345345');
+INSERT INTO `engine_numbers` (`engine_number`, `registration_number`) VALUES('343BDSN452', 'MN43LLJ23');
+INSERT INTO `engine_numbers` (`engine_number`, `registration_number`) VALUES('HGFDSN452', 'XDC3453');
 
 -- --------------------------------------------------------
 
@@ -140,22 +131,18 @@ INSERT INTO `engine_numbers` (`engine_number`, `registration_number`) VALUES
 -- Table structure for table `user_cards`
 --
 
-CREATE TABLE IF NOT EXISTS `user_cards` (
+CREATE TABLE `user_cards` (
   `card_id` bigint(20) NOT NULL,
   `card_name` varchar(20) DEFAULT NULL,
   `username` varchar(20) DEFAULT NULL,
-  `card_number` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`card_id`),
-  KEY `username` (`username`),
-  KEY `card_number` (`card_number`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `card_number` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user_cards`
 --
 
-INSERT INTO `user_cards` (`card_id`, `card_name`, `username`, `card_number`) VALUES
-(5643234, 'card', 'aa', 2343131231);
+INSERT INTO `user_cards` (`card_id`, `card_name`, `username`, `card_number`) VALUES(5643234, 'card', 'aa', 2343131231);
 
 -- --------------------------------------------------------
 
@@ -163,25 +150,23 @@ INSERT INTO `user_cards` (`card_id`, `card_name`, `username`, `card_number`) VAL
 -- Table structure for table `user_details`
 --
 
-CREATE TABLE IF NOT EXISTS `user_details` (
+CREATE TABLE `user_details` (
   `username` varchar(20) NOT NULL,
   `passwd` varchar(40) DEFAULT NULL,
   `first_name` varchar(20) DEFAULT NULL,
-  `last_name` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `last_name` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user_details`
 --
 
-INSERT INTO `user_details` (`username`, `passwd`, `first_name`, `last_name`) VALUES
-('aa', '4124bc0a9335c27f086f24ba207a4912', 'aa', 'bb'),
-('fsda', 'd29aaa0b9cd402b4bfe2395a805f9ada', 'klafvxv', 'ergrwff'),
-('nbvnc', '5c56fc99fe03e32897a4b562b502cd81', 'hnxvxxc', 'ytrhrth'),
-('vcbcb', '4124bc0a9335c27f086f24ba207a4912', 'bcvbc', 'xcser'),
-('vnbvncbcb', '4124bc0a9335c27f086f24ba207a4912', 'rbtbbcvbc', 'xxczxcser'),
-('xcvvx', 'c2a82dd938f5d722f574e94f42f60bca', 'sdfa', 'bhgdbdf');
+INSERT INTO `user_details` (`username`, `passwd`, `first_name`, `last_name`) VALUES('aa', '4124bc0a9335c27f086f24ba207a4912', 'aa', 'bb');
+INSERT INTO `user_details` (`username`, `passwd`, `first_name`, `last_name`) VALUES('fsda', 'd29aaa0b9cd402b4bfe2395a805f9ada', 'klafvxv', 'ergrwff');
+INSERT INTO `user_details` (`username`, `passwd`, `first_name`, `last_name`) VALUES('nbvnc', '5c56fc99fe03e32897a4b562b502cd81', 'hnxvxxc', 'ytrhrth');
+INSERT INTO `user_details` (`username`, `passwd`, `first_name`, `last_name`) VALUES('vcbcb', '4124bc0a9335c27f086f24ba207a4912', 'bcvbc', 'xcser');
+INSERT INTO `user_details` (`username`, `passwd`, `first_name`, `last_name`) VALUES('vnbvncbcb', '4124bc0a9335c27f086f24ba207a4912', 'rbtbbcvbc', 'xxczxcser');
+INSERT INTO `user_details` (`username`, `passwd`, `first_name`, `last_name`) VALUES('xcvvx', 'c2a82dd938f5d722f574e94f42f60bca', 'sdfa', 'bhgdbdf');
 
 -- --------------------------------------------------------
 
@@ -189,24 +174,21 @@ INSERT INTO `user_details` (`username`, `passwd`, `first_name`, `last_name`) VAL
 -- Table structure for table `user_emails`
 --
 
-CREATE TABLE IF NOT EXISTS `user_emails` (
+CREATE TABLE `user_emails` (
   `email` varchar(30) NOT NULL,
-  `username` varchar(30) NOT NULL,
-  PRIMARY KEY (`email`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `username` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user_emails`
 --
 
-INSERT INTO `user_emails` (`email`, `username`) VALUES
-('a@gmail.com', 'aa'),
-('adfsf@gmail.com', 'fsda'),
-('nbnv@gmail.com', 'nbvnc'),
-('yut@gmail.com', 'vcbcb'),
-('yerwewut@gmail.com', 'vnbvncbcb'),
-('sfsf@gmail.com', 'xcvvx');
+INSERT INTO `user_emails` (`email`, `username`) VALUES('a@gmail.com', 'aa');
+INSERT INTO `user_emails` (`email`, `username`) VALUES('adfsf@gmail.com', 'fsda');
+INSERT INTO `user_emails` (`email`, `username`) VALUES('nbnv@gmail.com', 'nbvnc');
+INSERT INTO `user_emails` (`email`, `username`) VALUES('yut@gmail.com', 'vcbcb');
+INSERT INTO `user_emails` (`email`, `username`) VALUES('yerwewut@gmail.com', 'vnbvncbcb');
+INSERT INTO `user_emails` (`email`, `username`) VALUES('sfsf@gmail.com', 'xcvvx');
 
 -- --------------------------------------------------------
 
@@ -214,24 +196,21 @@ INSERT INTO `user_emails` (`email`, `username`) VALUES
 -- Table structure for table `user_phone_numbers`
 --
 
-CREATE TABLE IF NOT EXISTS `user_phone_numbers` (
+CREATE TABLE `user_phone_numbers` (
   `phone_number` bigint(20) NOT NULL,
-  `username` varchar(30) NOT NULL,
-  PRIMARY KEY (`phone_number`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `username` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user_phone_numbers`
 --
 
-INSERT INTO `user_phone_numbers` (`phone_number`, `username`) VALUES
-(2313312321323, 'aa'),
-(1234567890, 'fsda'),
-(31234325534, 'nbvnc'),
-(5645223444, 'vcbcb'),
-(9845223444, 'vnbvncbcb'),
-(8008098934223, 'xcvvx');
+INSERT INTO `user_phone_numbers` (`phone_number`, `username`) VALUES(2313312321323, 'aa');
+INSERT INTO `user_phone_numbers` (`phone_number`, `username`) VALUES(1234567890, 'fsda');
+INSERT INTO `user_phone_numbers` (`phone_number`, `username`) VALUES(31234325534, 'nbvnc');
+INSERT INTO `user_phone_numbers` (`phone_number`, `username`) VALUES(5645223444, 'vcbcb');
+INSERT INTO `user_phone_numbers` (`phone_number`, `username`) VALUES(9845223444, 'vnbvncbcb');
+INSERT INTO `user_phone_numbers` (`phone_number`, `username`) VALUES(8008098934223, 'xcvvx');
 
 -- --------------------------------------------------------
 
@@ -239,21 +218,20 @@ INSERT INTO `user_phone_numbers` (`phone_number`, `username`) VALUES
 -- Table structure for table `vehicles`
 --
 
-CREATE TABLE IF NOT EXISTS `vehicles` (
+CREATE TABLE `vehicles` (
   `registration_number` varchar(11) NOT NULL,
   `vehicle_color` varchar(20) DEFAULT NULL,
-  `is_booked` tinyint(4) DEFAULT '0',
-  `model_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`registration_number`),
-  KEY `model_id` (`model_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `is_booked` tinyint(4) DEFAULT 0,
+  `model_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `vehicles`
 --
 
-INSERT INTO `vehicles` (`registration_number`, `vehicle_color`, `is_booked`, `model_id`) VALUES
-('MN43LLJ23', 'lkcvlv', 0, 4);
+INSERT INTO `vehicles` (`registration_number`, `vehicle_color`, `is_booked`, `model_id`) VALUES('CVB345345', 'cvxvsdf', 0, 4);
+INSERT INTO `vehicles` (`registration_number`, `vehicle_color`, `is_booked`, `model_id`) VALUES('MN43LLJ23', 'lkcvlv', 1, 4);
+INSERT INTO `vehicles` (`registration_number`, `vehicle_color`, `is_booked`, `model_id`) VALUES('XDC3453', 'cvxvsdf', 0, 4);
 
 -- --------------------------------------------------------
 
@@ -261,25 +239,119 @@ INSERT INTO `vehicles` (`registration_number`, `vehicle_color`, `is_booked`, `mo
 -- Table structure for table `vehicle_models`
 --
 
-CREATE TABLE IF NOT EXISTS `vehicle_models` (
-  `model_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `vehicle_models` (
+  `model_id` int(11) NOT NULL,
   `brand_name` varchar(20) DEFAULT NULL,
   `model_name` varchar(30) DEFAULT NULL,
   `vehicle_type` varchar(20) DEFAULT NULL,
   `hour_price` int(11) DEFAULT NULL,
   `day_price` int(11) DEFAULT NULL,
   `week_price` int(11) DEFAULT NULL,
-  `month_price` int(11) DEFAULT NULL,
-  PRIMARY KEY (`model_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=6 ;
+  `month_price` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `vehicle_models`
 --
 
-INSERT INTO `vehicle_models` (`model_id`, `brand_name`, `model_name`, `vehicle_type`, `hour_price`, `day_price`, `week_price`, `month_price`) VALUES
-(4, 'xcvfs', 'vcvsdfsf', 'cvxsdfs', 300, 1000, 2300, 7000),
-(5, 'vcbcx', 'rewtewr', 'adsds', 34, 634, 1234, 6532);
+INSERT INTO `vehicle_models` (`model_id`, `brand_name`, `model_name`, `vehicle_type`, `hour_price`, `day_price`, `week_price`, `month_price`) VALUES(4, 'xcvfs', 'vcvsdfsf', 'cvxsdfs', 300, 1000, 2300, 7000);
+INSERT INTO `vehicle_models` (`model_id`, `brand_name`, `model_name`, `vehicle_type`, `hour_price`, `day_price`, `week_price`, `month_price`) VALUES(5, 'vcbcx', 'rewtewr', 'adsds', 34, 634, 1234, 6532);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `card_booking_details`
+--
+ALTER TABLE `card_booking_details`
+  ADD PRIMARY KEY (`booking_id`),
+  ADD KEY `registration_number` (`registration_number`),
+  ADD KEY `card_id` (`card_id`);
+
+--
+-- Indexes for table `card_details`
+--
+ALTER TABLE `card_details`
+  ADD PRIMARY KEY (`card_number`);
+
+--
+-- Indexes for table `cash_booking_details`
+--
+ALTER TABLE `cash_booking_details`
+  ADD PRIMARY KEY (`booking_id`),
+  ADD KEY `registration_number` (`registration_number`),
+  ADD KEY `username` (`username`);
+
+--
+-- Indexes for table `engine_numbers`
+--
+ALTER TABLE `engine_numbers`
+  ADD PRIMARY KEY (`engine_number`),
+  ADD UNIQUE KEY `registration_number` (`registration_number`);
+
+--
+-- Indexes for table `user_cards`
+--
+ALTER TABLE `user_cards`
+  ADD PRIMARY KEY (`card_id`),
+  ADD KEY `username` (`username`),
+  ADD KEY `card_number` (`card_number`);
+
+--
+-- Indexes for table `user_details`
+--
+ALTER TABLE `user_details`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `user_emails`
+--
+ALTER TABLE `user_emails`
+  ADD PRIMARY KEY (`email`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `user_phone_numbers`
+--
+ALTER TABLE `user_phone_numbers`
+  ADD PRIMARY KEY (`phone_number`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `vehicles`
+--
+ALTER TABLE `vehicles`
+  ADD PRIMARY KEY (`registration_number`),
+  ADD KEY `model_id` (`model_id`);
+
+--
+-- Indexes for table `vehicle_models`
+--
+ALTER TABLE `vehicle_models`
+  ADD PRIMARY KEY (`model_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user_cards`
+--
+ALTER TABLE `user_cards`
+  MODIFY `card_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5643235;
+
+--
+-- AUTO_INCREMENT for table `vehicle_models`
+--
+ALTER TABLE `vehicle_models`
+  MODIFY `model_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
@@ -329,3 +401,8 @@ ALTER TABLE `user_phone_numbers`
 --
 ALTER TABLE `vehicles`
   ADD CONSTRAINT `vehicles_ibfk_1` FOREIGN KEY (`model_id`) REFERENCES `vehicle_models` (`model_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

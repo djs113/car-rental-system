ab30c0462759631f45a811f09e5ec65415034bb3
djs113 <daniel.julius.sam.113@gmail.com>
2023-08-08 20:11:28 +0530

    fix: added error handling for invalid access

--- registered-user/payment/payment-script.php
+++ registered-user/payment/payment-script.php
@@ -12,72 +12,77 @@
     if ($conn->connect_error)
         die("Connection error".$conn->connect_error);
 
-    $_SESSION['ongoing_payment'] = TRUE;
-
     if ($_POST['payment_method'])
-        $payment_method = $_POST['payment_method'];
-    
-    // to retain value of payment method after leaving this page to add a new card
-    
-    if (!isset($_SESSION['payment_method']))
-        $_SESSION['payment_method'] = $payment_method;
-    else
-        $payment_method = $_SESSION['payment_method'];
-
-    $username = $_SESSION['login_user'];
-
-    if ($payment_method == 'card')
     {
-        $qry = "SELECT card_name, card_number FROM user_cards WHERE username='$username'";
-        $res_array = mysqli_query($conn, $qry);
+        $_SESSION['ongoing_payment'] = TRUE;
+        $payment_method = $_POST['payment_method'];
         
-        $num_of_cards = mysqli_num_rows($res_array);
+        // to retain value of payment method after leaving this page to add a new card
+        
+        if (!isset($_SESSION['payment_method']))
+            $_SESSION['payment_method'] = $payment_method;
+        else
+            $payment_method = $_SESSION['payment_method'];
 
-        if ($num_of_cards != 0)
+        $username = $_SESSION['login_user'];
+
+        if ($payment_method == 'card')
         {
-            if ($num_of_cards == 1)
+            $qry = "SELECT card_name, card_number FROM user_cards WHERE username='$username'";
+            $res_array = mysqli_query($conn, $qry);
+            
+            $num_of_cards = mysqli_num_rows($res_array);
+
+            if ($num_of_cards != 0)
             {
-                $res = mysqli_fetch_array($res_array);
-                $masked_card_number = "XXXXX".substr($res[1], -4);
-                
-                echo '
-                    <form action="process-payment-form.php" method="POST">
-                        <label for="card_name">Card Name: '.$res[0].'</label>
-                        <br><br>
-
-                        <label for="card_number">Card Number: '.$masked_card_number.'</label>
-                        <input type="hidden" id="card_number" name="card_number" value="'.$res[1].'" />
-                        <br><br>
-
-                        <input type="submit" value="Pay" />
-                    </form>
-                ';
-            } else
-            {
-                echo '
-                    <h2>Card Name (Card Number)</h2>
-                    <form action="process-payment-form.php" method="POST"> 
-                ';
-
-                while ($res = mysqli_fetch_array($res_array))
+                if ($num_of_cards == 1)
                 {
+                    $res = mysqli_fetch_array($res_array);
                     $masked_card_number = "XXXXX".substr($res[1], -4);
+                    
                     echo '
-                        <input type="radio" name="card" value="'.$res[1].'" />
-                        <label for="card">'.$res[0].' ('.$masked_card_number.')</label>
+                        <form action="process-payment-form.php" method="POST">
+                            <label for="card_name">Card Name: '.$res[0].'</label>
+                            <br><br>
+
+                            <label for="card_number">Card Number: '.$masked_card_number.'</label>
+                            <input type="hidden" id="card_number" name="card_number" value="'.$res[1].'" />
+                            <br><br>
+
+                            <input type="submit" value="Pay" />
+                        </form>
+                    ';
+                } else
+                {
+                    echo '
+                        <h2>Card Name (Card Number)</h2>
+                        <form action="process-payment-form.php" method="POST"> 
+                    ';
+
+                    while ($res = mysqli_fetch_array($res_array))
+                    {
+                        $masked_card_number = "XXXXX".substr($res[1], -4);
+                        echo '
+                            <input type="radio" name="card" value="'.$res[1].'" />
+                            <label for="card">'.$res[0].' ('.$masked_card_number.')</label>
+                        ';
+                    }
+
+                    echo '
+                            <input type="submit" value="Pay" />
+                        </form>  
                     ';
                 }
+            } else
+                echo 'No saved cards.';
 
-                echo '
-                        <input type="submit" value="Pay" />
-                    </form>  
-                ';
-            }
-        } else
-            echo 'No saved cards.';
-
-        echo '
-            <button><a href="/car-rental-system/registered-user/payment/cards/add-card.php">Add new card</a></button> 
-        ';
+            echo '
+                <button><a href="/car-rental-system/registered-user/payment/cards/add-card.php">Add new card</a></button> 
+            ';
+        }
+    } else
+    {
+        sleep(3);
+        header("location:/car-rental-system/registered-user/vehicle-search/vehicle-search-form.php");
     }
 ?>

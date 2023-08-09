39874b186ec2ebad9a187bc0ccaaf516048f3e0d
djs113 <daniel.julius.sam.113@gmail.com>
2023-08-09 21:00:20 +0530

    fix: completed fixing the logic to get the data of bookable vehicles

--- registered-user/vehicle-search/vehicle-search-script.php
+++ registered-user/vehicle-search/vehicle-search-script.php
@@ -15,39 +15,28 @@
     
         $given_drop_off_date = $_POST['drop_off_date'];
         $given_drop_off_time = $_POST['drop_off_time'];
+
+        $available_models = [];
     
         // unbooked vehicle details
     
-        $qry = "SELECT vehicle_models.model_name, vehicle_models.brand_name FROM vehicles LEFT JOIN vehicle_models ON vehicles.model_id = vehicle_models.model_id WHERE vehicles.is_booked = 0 GROUP BY vehicle_models.model_id";
+        $qry = "SELECT DISTINCT vehicle_models.model_name, vehicle_models.brand_name, vehicle_models.model_id FROM vehicles LEFT JOIN vehicle_models ON vehicles.model_id = vehicle_models.model_id WHERE vehicles.is_booked = 0 GROUP BY vehicle_models.model_id";
         $unbooked_vehicles_array = mysqli_query($conn, $qry);
-    
+
+        $unbooked_vehicles_count = mysqli_num_rows($unbooked_vehicles_array);
+
+        while ($res = mysqli_fetch_array($unbooked_vehicles_array))
+            array_push($available_models, $res[2]);
         
-        // currently booked vehicle details (that satisfies time period given by user)
-        
-        $qry = "SELECT DISTINCT vehicle_models.model_name, vehicle_models.brand_name, vehicle_models.model_id FROM vehicles LEFT JOIN vehicle_models ON 
-                vehicles.model_id = vehicle_models.model_id WHERE vehicles.is_booked = 0 GROUP BY vehicle_models.model_id
-                UNION
-                SELECT DISTINCT vehicle_models.model_name, vehicle_models.brand_name, vehicle_models.model_id FROM vehicles LEFT JOIN card_booking_details
-                ON vehicles.registration_number = card_booking_details.registration_number LEFT JOIN vehicle_models ON 
-                vehicles.model_id = vehicle_models.model_id WHERE ('$given_drop_off_date' < card_booking_details.pick_up_date OR 
-                '$given_pick_up_date' > card_booking_details.drop_off_date) 
-                UNION 
-                SELECT DISTINCT vehicle_models.model_name, vehicle_models.brand_name, vehicle_models.model_id FROM vehicles LEFT JOIN 
-                card_booking_details ON vehicles.registration_number = card_booking_details.registration_number LEFT JOIN 
-                vehicle_models ON vehicles.model_id = vehicle_models.model_id WHERE 
-                (('$given_drop_off_date' < card_booking_details.pick_up_date) OR 
-                ('$given_pick_up_date' > card_booking_details.drop_off_date))";
+        // details of currently booked vehicles that satisfies given booking period details
             
         $model_qry = "SELECT model_id FROM vehicle_models";
-        $model_res_array = mysqli_query($conn, $qry);
-
-        $available_models = [];
-        
+        $model_res_array = mysqli_query($conn, $model_qry);
 
         while ($model_res = mysqli_fetch_array($model_res_array))
         {
             $unbookable_vehicles = [];
-            $model_id = $model_res['model_id'];
+            $model_id = $model_res[0];
 
             $qry = "SELECT vehicles.registration_number, card_booking_details.pick_up_date, 
                 card_booking_details.pick_up_time, card_booking_details.drop_off_date, card_booking_details.drop_off_time FROM 
@@ -70,27 +59,29 @@
                 $pick_up_time = $vehicles_res[2];
                 $drop_off_date = $vehicles_res[3];
                 $drop_off_time = $vehicles_res[4];
-
+                
                 if (!(($given_drop_off_date < $pick_up_date) || ($given_pick_up_date > $drop_off_date)))
                     array_push($unbookable_vehicles, $registration_number);
                 else
+                {
+                    foreach ($unbookable_vehicles as $unbookable_vehicle)
                     {
-                        foreach ($unbookable_vehicles as $unbookable_vehicle)
+                        if ($registration_number != $unbookable_vehicle)
                         {
-                            if ($registration_number != $unbookable_vehicle)
-                            {
+                            if (!(in_array($model_id, $available_models)))    
                                 array_push($available_models, $model_id);
-                                $flag = 0;
-                                break;
-                            }
+                        
+                            $flag = 0;
+                            break;
                         }
                     }
+                }
             }
         }
 
         $number_of_available_models = count($available_models);
 
-        if ($number_of_available_models == 0)
+        if (($unbooked_vehicles_count == 0) && ($number_of_available_models == 0))
         {
             $_SESSION['booking_ongoing'] = FALSE;
 
@@ -106,8 +97,11 @@
                     <th>Model Name</th>
                     <th>Brand Name</th>
             ';
-    
-            while ($res = mysqli_fetch_row($vehicles_array))
+
+            if ($unbooked_vehicles_count != 0)
+            {
+
+                while ($res = mysqli_fetch_array($unbooked_vehicles_array))
                 {
                     echo '
                         <tr>
@@ -126,11 +120,40 @@
                         </tr>
                     ';
                 }
+            } 
+            
+            if ($number_of_available_models != 0)
+            {
+                foreach ($available_models as $available_model_id)
+                {
+                    $qry = 'SELECT model_name, brand_name FROM vehicle_models WHERE model_id = '.$available_model_id;
+                    
+                    $res_array = mysqli_query($conn, $qry);
+                    $res = mysqli_fetch_array($res_array);
+
+                    echo '
+                        <tr>
+                            <td>'.$res[0].'</td>
+                            <td>'.$res[1].'</td>
+                            <td>
+                                <form action="/car-rental-system/registered-user/booking/confirm-booking.php" method="POST">
+                                    <input type="hidden" name="model_id" id="model_id" value="'.$available_model_id.'" />
+                                    <input type="hidden" name="pick_up_date" id="pick_up_date" value="'.$given_pick_up_date.'" />
+                                    <input type="hidden" name="pick_up_time" id="pick_up_time" value="'.$given_pick_up_time.'" />
+                                    <input type="hidden" name="drop_off_date" id="drop_off_date" value="'.$given_drop_off_date.'" />
+                                    <input type="hidden" name="drop_off_time" id="drop_off_time" value="'.$given_drop_off_time.'" />
+                                    <input type="submit" value="Book" />
+                                </form>
+                            </td>
+                        </tr>
+                    ';
+                }
+            }
+            
+            echo '
+                </table>
+            ';
         }
-    
-        echo '
-            </table>
-        ';
     } else
     {
         sleep(3);

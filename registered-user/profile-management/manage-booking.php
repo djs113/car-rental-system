b7efdbd252e9f2c7eafeed8997f4cd40838f11d1
djs113 <daniel.julius.sam.113@gmail.com>
2023-08-11 23:34:25 +0530

    feat: completed functionality to view details of individual bookings

--- registered-user/profile-management/manage-booking.php
+++ registered-user/profile-management/manage-booking.php
@@ -77,9 +77,61 @@
         } else
         {
             $qry = "SELECT cash_booking_details.*, vehicles.registration_number, vehicle_models.brand_name, 
-                    vehicle_models.model_name FROM cash_booking_detaials LEFT JOIN vehicles ON 
+                    vehicle_models.model_name FROM cash_booking_details LEFT JOIN vehicles ON 
                     cash_booking_details.registration_number = vehicles.registration_number LEFT JOIN 
                     vehicle_models ON vehicles.model_id = vehicle_models.model_id WHERE booking_id = $booking_id";
-        }
-    }
+
+            $res_array = mysqli_query($conn, $qry);
+            $res = mysqli_fetch_array($res_array);
+
+            $booking_count = mysqli_num_rows($res_array);
+
+            if ($booking_count != 0)
+            {
+                echo '
+                    <label for="booking_id">Booking Id: </label> '.$res['booking_id'].'
+                    <br>
+                    <br>
+
+                    <label for="registration_number">Registration Number: </label> '.$res['registration_number'].'
+                    <br>
+                    <br>
+
+                    <label for="brand_name">Brand Name: </label> '.$res['brand_name'].'
+                    <br>
+                    <br>
+
+                    <label for="model_name">Model Name: </label> '.$res['model_name'].'
+                    <br>
+                    <br>
+
+                    <label for="pick_up_date">Pick up date: </label> '.$res['pick_up_date'].'
+                    <br>
+                    <br>
+
+                    <label for="pick_up_time">Pick up time: </label> '.$res['pick_up_time'].'
+                    <br>
+                    <br>
+
+                    <label for="drop_off_date">Drop off date: </label> '.$res['drop_off_date'].'
+                    <br>
+                    <br>
+
+                    <label for="drop_off_time">Drop off time: </label> '.$res['drop_off_time'].'
+                    <br>
+                    <br>
+
+                    <label for="payment_amount">Payment amount: </label> '.$res['payment_amount'].'
+                    <br>
+                    <br>
+            ';
+            } else
+            {
+                echo '
+                    No bookings
+                    <br><br>    
+                ';
+            }
+        } 
+    } 
 ?>

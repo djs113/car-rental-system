5d3ac41c60b38074db11906b66ba9d70a8bdc6ab
djs113 <daniel.julius.sam.113@gmail.com>
2023-08-12 22:15:57 +0530

    feat: completed error handling

--- registered-user/profile-management/view-bookings.php
+++ registered-user/profile-management/view-bookings.php
@@ -17,7 +17,28 @@
     $qry = "SELECT card_id FROM user_cards WHERE username = '$username'";
     $res_array = mysqli_query($conn, $qry);
 
-    echo '
+    $count_qry = "SELECT COUNT(*) FROM card_booking_details LEFT JOIN user_cards ON 
+                    card_booking_details.card_id = user_cards.card_id WHERE user_cards.username = '$username'";
+
+    $card_count_res_array = mysqli_query($conn, $count_qry);
+    $card_count_res = mysqli_fetch_array($card_count_res_array);
+
+    $count_qry = "SELECT COUNT(*) FROM cash_booking_details WHERE username = '$username'";
+
+    $cash_count_res_array = mysqli_query($conn, $count_qry);
+    $cash_count_res = mysqli_fetch_array($card_count_res_array);
+
+    if (is_null($card_count_res))
+        $card_count_res[0] = 0;
+    
+    if (is_null($cash_count_res))
+        $cash_count_res[0] = 0;
+
+    $total_bookings = $card_count_res[0] + $cash_count_res[0];
+
+    if ($total_bookings != 0)
+    {
+        echo '
         <table border="1"> 
             <tr>
                 <th>Booking ID</th>
@@ -25,43 +46,52 @@
                 <th>Model Name</th>
                 <th>Pick up date</th>
             </tr>    
-    ';
+        ';
     
-    while ($res = mysqli_fetch_array($res_array))
-    {
-        $card_id = $res[0];
-
-        $qry = "SELECT card_booking_details.booking_id, vehicle_models.brand_name, 
-                vehicle_models.model_name, card_booking_details.pick_up_date FROM card_booking_details LEFT JOIN vehicles ON 
-                card_booking_details.registration_number = vehicles.registration_number LEFT JOIN 
-                vehicle_models ON vehicles.model_id = vehicle_models.model_id WHERE 
-                card_booking_details.card_id = $card_id
-                UNION
-                SELECT cash_booking_details.booking_id, vehicle_models.brand_name, vehicle_models.model_name, 
-                cash_booking_details.pick_up_date FROM cash_booking_details LEFT JOIN vehicles ON 
-                cash_booking_details.registration_number = vehicles.registration_number LEFT JOIN 
-                vehicle_models ON vehicles.model_id = vehicle_models.model_id WHERE username = '$username'";
-        
-        $res_array = mysqli_query($conn, $qry);
-
         while ($res = mysqli_fetch_array($res_array))
         {
-            echo '
-                <tr>
-                    <td>'.$res[0].'</td>
-                    <td>'.$res[1].'</td>
-                    <td>'.$res[2].'</td>
-                    <td>'.$res[3].'</td>
-                    <td><a href="/car-rental-system/registered-user/profile-management/manage-booking.php?booking_id='.$res[0].'">View Booking</a></td>
-                </tr> 
-            ';
+            $card_id = $res[0];
+
+            $qry = "SELECT card_booking_details.booking_id, vehicle_models.brand_name, 
+                    vehicle_models.model_name, card_booking_details.pick_up_date FROM card_booking_details LEFT JOIN vehicles ON 
+                    card_booking_details.registration_number = vehicles.registration_number LEFT JOIN 
+                    vehicle_models ON vehicles.model_id = vehicle_models.model_id WHERE 
+                    card_booking_details.card_id = $card_id
+                    UNION
+                    SELECT cash_booking_details.booking_id, vehicle_models.brand_name, vehicle_models.model_name, 
+                    cash_booking_details.pick_up_date FROM cash_booking_details LEFT JOIN vehicles ON 
+                    cash_booking_details.registration_number = vehicles.registration_number LEFT JOIN 
+                    vehicle_models ON vehicles.model_id = vehicle_models.model_id WHERE username = '$username'";
+            
+            $res_array = mysqli_query($conn, $qry);
+
+            while ($res = mysqli_fetch_array($res_array))
+            {
+                echo '
+                    <tr>
+                        <td>'.$res[0].'</td>
+                        <td>'.$res[1].'</td>
+                        <td>'.$res[2].'</td>
+                        <td>'.$res[3].'</td>
+                        <td><a href="/car-rental-system/registered-user/profile-management/manage-booking.php?booking_id='.$res[0].'">View Booking</a></td>
+                    </tr> 
+                ';
+            }
         }
+
+        echo '
+            </table>
+            <br><br>
+        ';
+    } else
+    {
+        echo '
+            No bookings available!
+            <br><br>     
+        ';
     }
 
-    echo '
-        </table>
-        <br><br>
-
+    echo '   
         <button><a href="/car-rental-system/registered-user/vehicle-search/vehicle-search-form.php">Go back</a></button> 
     ';
 ?>

67206bb9db0be3b688e6a7beed35d9211620074e
djs113 <daniel.julius.sam.113@gmail.com>
2023-08-14 18:21:00 +0530

    feat: started functionality to modify contact details

---
+++ admin/contact-management/modify-contact-details/modify-contact-details-form.php
@@ -0,0 +1,16 @@
+<?php
+    session_start();
+
+    if (!isset($_SESSION['login_admin']))
+    {
+        header("location:/car-rental-system/admin/admin-login/admin-login-form.php");
+        exit;
+    }
+
+    $conn = mysqli_connect('localhost', 'root', '', 'car_rental_system');
+
+    if ($conn->connect_error)
+        die("Connection failed<br>Connection Error: ".$conn->connect_error);
+
+    
+?>

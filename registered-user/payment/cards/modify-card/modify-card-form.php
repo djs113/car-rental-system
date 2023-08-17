895f0d748c07d5db1394e62d051a2426700c2b1b
djs113 <daniel.julius.sam.113@gmail.com>
2023-08-17 17:10:24 +0530

    feat: started functionality to modify card details

---
+++ registered-user/payment/cards/modify-card/modify-card-form.php
@@ -0,0 +1,14 @@
+<?php
+    session_start();
+
+    if (!isset($_SESSION['login_user']))
+    {
+        header("location:/car-rental-system/registered-user/user-login/user-login-page.html");
+        exit;
+    }
+
+    $conn = mysqli_connect('localhost', 'root', '', 'car_rental_system');
+
+    if ($conn->connect_error)
+        die("Connection error".$conn->connect_error);
+?>

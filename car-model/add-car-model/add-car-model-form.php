89b5e596a9a4f46ea875210af04d194e6fb6a258
djs113 <daniel.julius.sam.113@gmail.com>
2023-08-20 13:17:32 +0530

    feat: completed form validation for form to add car model details

--- car-model/add-car-model/add-car-model-form.php
+++ car-model/add-car-model/add-car-model-form.php
@@ -14,8 +14,78 @@
         }
     ?>
     <body>
+        <script>
+            function formValidate()
+            {
+                var brand_name = document.getElementById("brand_name").value;
+                var model_name = document.getElementById("model_name").value;
+                var vehicle_type = document.getElementById("vehicle_type").value;
+                var hour_price = document.getElementById("hour_price").value;
+                var day_price = document.getElementById("day_price").value;
+                var week_price = document.getElementById("week_price").value;
+                var month_price = document.getElementById("month_price").value;
+
+                if (brand_name == "")
+                {
+                    alert("Enter brand name");
+                    return false;
+                }
+
+                if (model_name == "")
+                {
+                    alert("Enter model name");
+                    return false;
+                }
+
+                if (vehicle_type == "")
+                {
+                    alert("Enter vehicle type");
+                    return false;
+                }
+
+                if (hour_price == "")
+                {
+                    alert("Enter hour price");
+                    return false;
+                } else if (isNaN(hour_price))
+                {
+                    alert("Hour price should be a number");
+                    return false;
+                }
+
+                if (day_price == "")
+                {
+                    alert("Enter day price");
+                    return false;
+                } else if (isNaN(day_price))
+                {
+                    alert("Day price should be a number");
+                    return false;
+                }
+
+                if (week_price == "")
+                {
+                    alert("Enter week price");
+                    return false;
+                } else if (isNaN(week_price))
+                {
+                    alert("Week price should be a number");
+                    return false;
+                }
+                
+                if (month_price == "")
+                {
+                    alert("Enter month price");
+                    return false;
+                } else if (isNaN(month_price))
+                {
+                    alert("Month price should be a number");
+                    return false;
+                }
+            }
+        </script>
         <h2><u>Add Car Model</u></h2>
-        <form action="add-car-model.php" method="POST">
+        <form action="add-car-model.php" method="POST" onsubmit="return formValidate()">
             <label for="brand name">Brand Name: </label><input type="text" name="brand_name" id="brand_name" />
             <br><br>
 

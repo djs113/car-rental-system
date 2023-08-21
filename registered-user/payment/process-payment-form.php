721bf4d6180c9c0b0440766e56858e089ebb1321
djs113 <daniel.julius.sam.113@gmail.com>
2023-08-21 15:29:14 +0530

    feat: completed form validation for process-payment-form.php

--- registered-user/payment/process-payment-form.php
+++ registered-user/payment/process-payment-form.php
@@ -15,12 +15,28 @@
     if ($_POST['card_number'])
     {
         echo '
-        <form action="confirm-payment.php" method="POST">
-            <label for="otp">Enter the otp: </label><input type="text" id="otp" name="otp" />
-            <br><br>
-            
-            <input type="submit" value="Submit" />
-        </form>
+            <script type="text/javascript">
+                function formValidate()
+                {
+                    var otp = document.getElementById("otp").value;
+
+                    if (otp == "")
+                    {
+                        alert("Enter the otp");
+                        return false;
+                    } else if (isNaN(otp))
+                    {
+                        alert("Otp must be a number");
+                        return false;
+                    }
+                }
+            </script>
+            <form action="confirm-payment.php" method="POST" onsubmit="return formValidate()">
+                <label for="otp">Enter the otp: </label><input type="text" id="otp" name="otp" />
+                <br><br>
+                
+                <input type="submit" value="Submit" />
+            </form>
         '; 
     } else
     {

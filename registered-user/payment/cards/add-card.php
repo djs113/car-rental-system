13bf000b6fe6ce157035eb16b5527ddbe0e6cf08
djs113 <daniel.julius.sam.113@gmail.com>
2023-08-22 15:14:02 +0530

    feat: completed form validation for add-card.php

--- registered-user/payment/cards/add-card.php
+++ registered-user/payment/cards/add-card.php
@@ -13,7 +13,44 @@
         die("Connection error".$conn->connect_error);
 
     echo '
-        <form action="card-addition-script.php" method="POST">
+        <script type="text/javascript">
+            function formValidate()
+            {
+                var name_on_card = document.getElementById("name_on_card").value;
+                var expiry_date = document.getElementById("expiry_date").value;
+                var card_name = document.getElementById("card_name").value;
+
+                if (name_on_card == "")
+                {
+                    alert("Enter name on card");
+                    return false;
+                } 
+
+                if (expiry_date == "")
+                {
+                    alert("Enter expiry date");
+                    return false;
+                } else 
+                {
+                    var entered_date = new Date(expiry_date);
+                    var current_date = new Date();
+                    
+                    if (entered_date <= current_date.getTime())
+                    {
+                        alert("Enter valid expiry date");
+                        return false;
+                    } 
+                }
+
+                if (card_name == "")
+                {
+                    alert("Enter card name");
+                    return false;
+                }
+            } 
+        </script>
+        
+        <form action="card-addition-script.php" method="POST" onsubmit="return formValidate()">
             <label for="card_number">Card Number: </label><input type="text" id="card_number" name="card_number" />
             <br><br>
                 <label for="name_on_card">Name on Card: </label><input type="text" id="name_on_card" name="name_on_card" />

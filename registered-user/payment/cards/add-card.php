16e8c2d71ed5ce558bbeaafdc8fc2b679f1e80f3
djs113 <daniel.julius.sam.113@gmail.com>
2023-08-22 15:19:52 +0530

    feat: added form validation for card number field in add-card.php

--- registered-user/payment/cards/add-card.php
+++ registered-user/payment/cards/add-card.php
@@ -16,9 +16,20 @@
         <script type="text/javascript">
             function formValidate()
             {
+                var card_number = document.getElementById("card_number").value;
                 var name_on_card = document.getElementById("name_on_card").value;
                 var expiry_date = document.getElementById("expiry_date").value;
                 var card_name = document.getElementById("card_name").value;
+
+                if (card_number == "")
+                {
+                    alert("Enter card number");
+                    return false;
+                } else if (isNaN(card_number))
+                {
+                    alert("Card number should be a number");
+                    return false;
+                }
 
                 if (name_on_card == "")
                 {
@@ -49,18 +60,19 @@
                 }
             } 
         </script>
-        
+
         <form action="card-addition-script.php" method="POST" onsubmit="return formValidate()">
             <label for="card_number">Card Number: </label><input type="text" id="card_number" name="card_number" />
             <br><br>
-                <label for="name_on_card">Name on Card: </label><input type="text" id="name_on_card" name="name_on_card" />
-                <br><br>
 
-                <label for="expiry_date">Expiry date: </label><input type="date" id="expiry_date" name="expiry_date" />
-                <br><br>
+            <label for="name_on_card">Name on Card: </label><input type="text" id="name_on_card" name="name_on_card" />
+            <br><br>
 
-                <label for="card_name">Card Name: </label><input type="text" id="card_name" name="card_name" />
-                <br><br>
+            <label for="expiry_date">Expiry date: </label><input type="date" id="expiry_date" name="expiry_date" />
+            <br><br>
+
+            <label for="card_name">Card Name: </label><input type="text" id="card_name" name="card_name" />
+            <br><br>
 
                 <input type="submit" value="Add card" />
         </form> 

489cd8df985783b410a0fc53f18c76a4bd3b08a1
djs113 <daniel.julius.sam.113@gmail.com>
2023-08-27 20:42:56 +0530

    feat: added css styles to admin-login-form.php

--- admin/admin-login/admin-login-form.php
+++ admin/admin-login/admin-login-form.php
@@ -3,23 +3,25 @@
         <title>
             Admin Login Page
         </title>
+        <link rel="stylesheet" type="text/css" href="admin-login-form-css.css">
     </head>
-    <body style="background-image: url(/car-rental-system/images/cars-background.webp); background-size: cover;">
-        <div style="text-align: center; width: 100%; margin-top: 100px; float: left">
-            <div style="width: 40%; float: left; min-height: 200px; background-color: #a0cdf9; margin-left: 30%;">
-                <h2>ADMIN LOGIN</h2>
-                <p><strong>WELCOME TO THE LOGIN PAGE FOR ADMIN</strong></p>
+    <body>
+        <div class="heading">
+            <h1>ADMIN LOGIN</h1>
+            <h2><strong>WELCOME TO THE LOGIN PAGE FOR ADMIN</strong></h2>
+        </div>
 
-                <form action="/car-rental-system/admin/admin-login/admin-login.php" method="POST">
-                    <label for="username"><b>Username: </b></label><input type="text" name="username" id="username" />
-                    <br><br>
+        <div class="main">
+            <form action="/car-rental-system/admin/admin-login/admin-login.php" method="POST">
+                <label for="username">Username: </label><input type="text" name="username" id="username" />
+                <br><br>
 
-                    <label for="password"><b>Password: </b></label><input type="password" name="passwd" id="passwd" />
-                    <br><br>
+                <label for="password">Password: </label><input type="password" name="passwd" id="passwd" />
+                <br><br>
 
-                    <input type="submit" value="Login" />
-                </form>
-            </div>
-        </div>    
+                <input type="submit" class="submit" value="Login" />
+            </form>
+        </div>
     </body>
 </html>
+

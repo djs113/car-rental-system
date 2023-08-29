9691741d9ab30c20edc1552a1216d248d7e6b67f
djs113 <daniel.julius.sam.113@gmail.com>
2023-08-29 10:17:48 +0530

    feat: added css styles and a link to go back to delete-car.php

--- car/delete-car/delete-car.php
+++ car/delete-car/delete-car.php
@@ -12,6 +12,12 @@
     if ($conn->connect_error)
         die("Connection error".$conn->connect_error);
 
+    echo '
+        <link rel="stylesheet" type="text/css" href="/car-rental-system/car/delete-car/delete-car-css.css">
+        <div class="main">
+            <p>
+    ';
+
     $registration_number = $_POST['registration_number'];
 
     $qry = "DELETE FROM card_booking_details WHERE registration_number = '$registration_number';";
@@ -21,7 +27,13 @@
     $qry .= "DELETE FROM vehicles WHERE registration_number = '$registration_number';";
  
     if ($conn->multi_query($qry) == TRUE)
-        echo "Car successfully deleted.<br>";
+        echo "Car successfully deleted.<br><br>";
     else
-        echo "Error in deletion of car.<br>Error: ".$conn->error;
+        echo "Error in deletion of car.<br>Error: ".$conn->error."<br><br>";
+    
+    echo '
+            </p>
+            <a href="/car-rental-system/car/view-car/view-all-cars.php">Go back</a> 
+        </div>    
+    ';
 ?>

b4269a4f2f69035a2b5981b7fe6aad960ee79125
djs113 <daniel.julius.sam.113@gmail.com>
2023-08-29 08:21:19 +0530

    feat: started adding css styles to modify-car-css.css

--- car/modify-car/modify-car.php
+++ car/modify-car/modify-car.php
@@ -12,6 +12,11 @@
     if ($conn->connect_error)
         die("Connection error".$conn->connect_error);
 
+    echo '
+        <link rel="stylesheet" type="text/css" href="/car-rental-system/car/modify-car/modify-car-css.css"> 
+        <div class="main">  
+    ';
+
     $registration_number = $_POST['registration_number'];
     $vehicle_color = $_POST['vehicle_color'];
     $is_booked = $_POST['is_booked'];
@@ -22,8 +27,12 @@
     $qry .= "UPDATE engine_numbers SET engine_number = '$engine_number' WHERE registration_number = '$registration_number'";
     
     if ($conn->multi_query($qry) == TRUE)
-        echo "Car successfully modified.<br>";
+        echo "Car successfully modified.<br><br>";
     else
         echo "Error in modification of car.<br>Error: ".$conn->error;
-    
+   
+    echo '
+            <a href="/car-rental-system/admin/admin-home/admin-home-page.php">Go back</a>
+        </div>
+    ';
 ?>

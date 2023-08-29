55e61587c3f085e796892891d7d6c16619ed6d5e
djs113 <daniel.julius.sam.113@gmail.com>
2023-08-29 11:12:36 +0530

    feat: added css styles and a link to go back to add-car-model.php

--- car-model/add-car-model/add-car-model.php
+++ car-model/add-car-model/add-car-model.php
@@ -20,10 +20,22 @@
     if ($conn->connect_error)
         die("Connection failed<br>Connection Error: ".$conn->connect_error);
 
+    echo '
+        <link rel="stylesheet" type="text/css" href="/car-rental-system/car-model/add-car-model/add-car-model-css.css">    
+        <div class="main">
+            <p>   
+    ';
+
     $qry = "INSERT INTO vehicle_models (brand_name, model_name, vehicle_type, hour_price, day_price, week_price, month_price) VALUES ('$brand_name', '$model_name', '$vehicle_type', $hour_price, $day_price, $week_price, $month_price)";
     
     if ($conn->query($qry) === TRUE)
-        echo "Car model successfuly added";
+        echo "Car model successfuly added<br>";
     else
         echo "Error in addition of car model<br>Error: ".$conn->error."<br>";
+
+    echo '
+            </p>
+            <a href="/car-rental-system/car-model/add-car-model/add-car-model-form.php">Go back</a> 
+        </div> 
+    ';
 ?>

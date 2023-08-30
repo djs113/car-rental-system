5df9de950599f76eebf436b72e7aa0f835d8c4e6
djs113 <daniel.julius.sam.113@gmail.com>
2023-08-30 15:20:35 +0530

    feat: added message to indicate that no users are added

--- admin/view-user/view-all-users.php
+++ admin/view-user/view-all-users.php
@@ -20,8 +20,17 @@
                 <title>
                     View Users       
                 </title>
+                <link rel="stylesheet" href="/car-rental-system/admin/view-user/view-all-users-css.css">
             </head>
             <body>
+                <div class="main"> 
+    ';
+
+    $user_count = mysqli_num_rows($res_array);
+
+    if ($user_count != 0)
+    {
+        echo '
             <h2><u>View Users</u></h2>
             
             <table cellspacing="3" cellpadding="3" border="1">
@@ -33,27 +42,39 @@
                 <th>Email</th>  
                 <th>Phone Number</th>
             </tr>
-    ';
-    
-    while ($res = mysqli_fetch_array($res_array))
-    {
-        $username = $res['username'];
+        ';
+        
+        while ($res = mysqli_fetch_array($res_array))
+        {
+            $username = $res['username'];
 
-        echo '<tr>';
-        echo '<td>'.$res['username'].'</td>';
-        echo '<td>'.$res['passwd'].'</td>';
-        echo '<td>'.$res['first_name'].'</td>';
-        echo '<td>'.$res['last_name'].'</td>';
-        echo '<td>'.$res['email'].'</td>';
-        echo '<td>'.$res['phone_number'].'</td>';
-        echo '<td><a href="/car-rental-system/admin/modify-user/modify-user-form.php?username='.$username.'">Edit User</a></td>';    
-        echo '<td><a href="/car-rental-system/admin/delete-user/delete-user-form.php?username='.$username.'">Delete User</a></td>';    
-        echo '</tr>';
+            echo '<tr>';
+            echo '<td>'.$res['username'].'</td>';
+            echo '<td>'.$res['passwd'].'</td>';
+            echo '<td>'.$res['first_name'].'</td>';
+            echo '<td>'.$res['last_name'].'</td>';
+            echo '<td>'.$res['email'].'</td>';
+            echo '<td>'.$res['phone_number'].'</td>';
+            echo '<td><a href="/car-rental-system/admin/modify-user/modify-user-form.php?username='.$username.'">Edit User</a></td>';    
+            echo '<td><a href="/car-rental-system/admin/delete-user/delete-user-form.php?username='.$username.'">Delete User</a></td>';    
+            echo '</tr>';
+        }
+
+        echo '
+            </table> 
+        ';
+    } else {
+        echo '
+            <p>No users are added</p>
+            <br>
+        ';
     }
 
-    echo '</table>
-        </body>
-    </html>
+    echo '
+                    <a href="/car-rental-system/admin/admin-home/admin-home-page.php">Go back</a> 
+                </div>
+            </body>
+        </html>
     ';
 
     $conn->close();

f350f82372ade10e49ecebc29950d540badf2a8f
djs113 <daniel.julius.sam.113@gmail.com>
2023-09-01 14:25:23 +0530

    feat: added css styles to add-car-script.php

--- car/add-car/add-car-script.php
+++ car/add-car/add-car-script.php
@@ -18,34 +18,76 @@
             <p>
     ';
 
-
     $brand_name = $_POST['brand_name'];
     $model_name = $_POST['model_name'];
     $registration_number = $_POST['registration_number'];
     $engine_number = $_POST['engine_number'];
     $vehicle_color = $_POST['vehicle_color'];
 
-    $qry = "SELECT model_id FROM vehicle_models WHERE brand_name = '$brand_name' AND 
-            model_name = '$model_name'";
-    
-    $res_array = mysqli_query($conn, $qry);
+    $reg_no_count_qry = "SELECT COUNT(*) FROM vehicles WHERE registration_number = '$registration_number'";
 
-    $res = mysqli_fetch_array($res_array);
-    $model_id = $res['model_id'];
+    $reg_no_res_array = mysqli_query($conn, $reg_no_count_qry);
+    $reg_no_res = mysqli_fetch_array($reg_no_res_array);
 
-    $qry = "INSERT INTO vehicles (registration_number, vehicle_color, model_id) VALUES 
-    ('$registration_number', '$vehicle_color', '$model_id');";
-    
-    $qry .= "INSERT INTO engine_numbers VALUES ('$engine_number', '$registration_number');";
-    
-    if ($conn->multi_query($qry) == TRUE)
-        echo 'Successfully added car!';
-    else
-        echo 'Error in addition of car';
+    $eng_no_count_qry = "SELECT COUNT(*) FROM engine_numbers WHERE engine_number = '$engine_number'";
 
-    echo '
-            </p>
-            <a href="/car-rental-system/car/add-car/add-car-form.php">Go back</a>
-        </div>    
-    '; 
+    $eng_no_res_array = mysqli_query($conn, $eng_no_count_qry);
+    $eng_no_res = mysqli_fetch_array($eng_no_res_array);
+
+    if (is_null($reg_no_res))
+        $reg_no_res[0] = 0;
+
+    if (is_null($eng_no_res))
+        $eng_no_res[0] = 0;    
+
+    if (($eng_no_res[0] == 0) && ($reg_no_res[0] == 0))
+    {
+        $qry = "SELECT model_id FROM vehicle_models WHERE brand_name = '$brand_name' AND 
+        model_name = '$model_name'";
+
+        $res_array = mysqli_query($conn, $qry);
+
+        $res = mysqli_fetch_array($res_array);
+        $model_id = $res['model_id'];
+
+        $qry = "INSERT INTO vehicles (registration_number, vehicle_color, model_id) VALUES 
+        ('$registration_number', '$vehicle_color', '$model_id');";
+
+        $qry .= "INSERT INTO engine_numbers VALUES ('$engine_number', '$registration_number');";
+
+        if ($conn->multi_query($qry) == TRUE)
+            echo 'Successfully added car!';
+        else
+            echo 'Error in addition of car';
+
+        echo '
+                </p>
+                <a href="/car-rental-system/car/add-car/add-car-form.php">Go back</a>
+            </div>    
+        ';
+    } else  
+    {
+        echo '
+            <span> 
+        ';
+        if ($reg_no_res[0] != 0)
+        {
+            echo '
+                Another vehicle with this registration number has already been added
+            ';
+        } else 
+        {
+            echo '
+                Another vehicle with this engine number has already been added 
+            ';
+        }
+
+        echo '
+                </span>
+                </p>
+                <br><br>
+                <a href="/car-rental-system/car/add-car/add-car-form.php">Go back</a>
+            </div>
+        ';
+    }
 ?>

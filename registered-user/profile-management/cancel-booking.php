994c877f358421864e642a754ff4d60ce9295ca6
djs113 <daniel.julius.sam.113@gmail.com>
2023-09-18 10:14:51 +0530

    fix: fixed position of go back button

--- registered-user/profile-management/cancel-booking.php
+++ registered-user/profile-management/cancel-booking.php
@@ -15,6 +15,7 @@
     echo '
         <link rel="stylesheet" type="text/css" href="/car-rental-system/registered-user/profile-management/cancel-booking-css.css"> 
         <div class="main">
+            <p>
     ';
 
     if (isset($_POST['booking_id']))
@@ -75,9 +76,10 @@
                 Error: '.$conn->error;  
         }
 
-        echo '
+        echo '  
+                </>
+                <a href="/car-rental-system/registered-user/profile-management/view-bookings.php">Go back</a> 
             </div>
-            <a href="/car-rental-system/registered-user/profile-management/view-bookings.php">Go back</a> 
         ';
     } else
         header("location:/car-rental-system/registered-user/profile-management/view-bookings.php"); 
